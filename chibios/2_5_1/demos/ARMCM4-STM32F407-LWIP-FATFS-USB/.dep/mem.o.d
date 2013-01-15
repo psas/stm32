36054fbc0dacacd81a014c9c08f523a7cfd3c2f9
kwilson <kwilson@corvine.net>
2013-01-15 13:23:53 -0800

    Chibios 2_5_1 board configuration and demo. Tarfile for x-compilers

---
+++ chibios/2_5_1/demos/ARMCM4-STM32F407-LWIP-FATFS-USB/.dep/mem.o.d
@@ -0,0 +1,163 @@
+build/obj/mem.o: ../../ext/lwip/src/core/mem.c \
+ ../../ext/lwip/src/include/lwip/opt.h lwipopts.h \
+ ../../ext/lwip/src/include/lwip/debug.h \
+ ../../ext/lwip/src/include/lwip/arch.h \
+ ../../os/various/lwip_bindings/arch/cc.h ../../os/kernel/include/ch.h \
+ chconf.h ../../os/ports/GCC/ARMCMx/chtypes.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/include/stddef.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/include/stdint.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/stdint.h \
+ ../../os/kernel/include/chlists.h ../../os/ports/GCC/ARMCMx/chcore.h \
+ ../../os/ports/GCC/ARMCMx/STM32F4xx/cmparams.h \
+ ../../os/ports/GCC/ARMCMx/chcore_v7m.h \
+ ../../os/ports/common/ARMCMx/nvic.h ../../os/kernel/include/chsys.h \
+ ../../os/kernel/include/chvt.h ../../os/kernel/include/chschd.h \
+ ../../os/kernel/include/chsem.h ../../os/kernel/include/chbsem.h \
+ ../../os/kernel/include/chmtx.h ../../os/kernel/include/chcond.h \
+ ../../os/kernel/include/chevents.h ../../os/kernel/include/chmsg.h \
+ ../../os/kernel/include/chmboxes.h ../../os/kernel/include/chmemcore.h \
+ ../../os/kernel/include/chheap.h ../../os/kernel/include/chmempools.h \
+ ../../os/kernel/include/chthreads.h ../../os/kernel/include/chdynamic.h \
+ ../../os/kernel/include/chregistry.h ../../os/kernel/include/chinline.h \
+ ../../os/kernel/include/chqueues.h ../../os/kernel/include/chstreams.h \
+ ../../os/kernel/include/chfiles.h ../../os/kernel/include/chdebug.h \
+ ../../ext/lwip/src/include/lwip/def.h \
+ ../../ext/lwip/src/include/lwip/mem.h \
+ ../../ext/lwip/src/include/lwip/sys.h \
+ ../../ext/lwip/src/include/lwip/err.h \
+ ../../os/various/lwip_bindings/arch/sys_arch.h \
+ ../../ext/lwip/src/include/lwip/stats.h \
+ ../../ext/lwip/src/include/lwip/memp.h \
+ ../../ext/lwip/src/include/lwip/memp_std.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/string.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/_ansi.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/newlib.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/config.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/machine/ieeefp.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/features.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/reent.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/_ansi.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/_types.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/machine/_types.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/machine/_default_types.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/lock.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/cdefs.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/string.h
+
+../../ext/lwip/src/include/lwip/opt.h:
+
+lwipopts.h:
+
+../../ext/lwip/src/include/lwip/debug.h:
+
+../../ext/lwip/src/include/lwip/arch.h:
+
+../../os/various/lwip_bindings/arch/cc.h:
+
+../../os/kernel/include/ch.h:
+
+chconf.h:
+
+../../os/ports/GCC/ARMCMx/chtypes.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/include/stddef.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/include/stdint.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/stdint.h:
+
+../../os/kernel/include/chlists.h:
+
+../../os/ports/GCC/ARMCMx/chcore.h:
+
+../../os/ports/GCC/ARMCMx/STM32F4xx/cmparams.h:
+
+../../os/ports/GCC/ARMCMx/chcore_v7m.h:
+
+../../os/ports/common/ARMCMx/nvic.h:
+
+../../os/kernel/include/chsys.h:
+
+../../os/kernel/include/chvt.h:
+
+../../os/kernel/include/chschd.h:
+
+../../os/kernel/include/chsem.h:
+
+../../os/kernel/include/chbsem.h:
+
+../../os/kernel/include/chmtx.h:
+
+../../os/kernel/include/chcond.h:
+
+../../os/kernel/include/chevents.h:
+
+../../os/kernel/include/chmsg.h:
+
+../../os/kernel/include/chmboxes.h:
+
+../../os/kernel/include/chmemcore.h:
+
+../../os/kernel/include/chheap.h:
+
+../../os/kernel/include/chmempools.h:
+
+../../os/kernel/include/chthreads.h:
+
+../../os/kernel/include/chdynamic.h:
+
+../../os/kernel/include/chregistry.h:
+
+../../os/kernel/include/chinline.h:
+
+../../os/kernel/include/chqueues.h:
+
+../../os/kernel/include/chstreams.h:
+
+../../os/kernel/include/chfiles.h:
+
+../../os/kernel/include/chdebug.h:
+
+../../ext/lwip/src/include/lwip/def.h:
+
+../../ext/lwip/src/include/lwip/mem.h:
+
+../../ext/lwip/src/include/lwip/sys.h:
+
+../../ext/lwip/src/include/lwip/err.h:
+
+../../os/various/lwip_bindings/arch/sys_arch.h:
+
+../../ext/lwip/src/include/lwip/stats.h:
+
+../../ext/lwip/src/include/lwip/memp.h:
+
+../../ext/lwip/src/include/lwip/memp_std.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/string.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/_ansi.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/newlib.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/config.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/machine/ieeefp.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/features.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/reent.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/_ansi.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/_types.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/machine/_types.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/machine/_default_types.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/lock.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/cdefs.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/string.h:
---
+++ chibios/2_5_1/demos/ARMCM4-STM32F407-LWIP-FATFS-USB/.dep/usb.o.d
@@ -0,0 +1,237 @@
+build/obj/usb.o: ../../os/hal/src/usb.c \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/string.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/_ansi.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/newlib.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/config.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/machine/ieeefp.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/features.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/reent.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/_ansi.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/_types.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/machine/_types.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/machine/_default_types.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/lock.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/include/stddef.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/cdefs.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/string.h \
+ ../../os/kernel/include/ch.h chconf.h \
+ ../../os/ports/GCC/ARMCMx/chtypes.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/include/stdint.h \
+ /opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/stdint.h \
+ ../../os/kernel/include/chlists.h ../../os/ports/GCC/ARMCMx/chcore.h \
+ ../../os/ports/GCC/ARMCMx/STM32F4xx/cmparams.h \
+ ../../os/ports/GCC/ARMCMx/chcore_v7m.h \
+ ../../os/ports/common/ARMCMx/nvic.h ../../os/kernel/include/chsys.h \
+ ../../os/kernel/include/chvt.h ../../os/kernel/include/chschd.h \
+ ../../os/kernel/include/chsem.h ../../os/kernel/include/chbsem.h \
+ ../../os/kernel/include/chmtx.h ../../os/kernel/include/chcond.h \
+ ../../os/kernel/include/chevents.h ../../os/kernel/include/chmsg.h \
+ ../../os/kernel/include/chmboxes.h ../../os/kernel/include/chmemcore.h \
+ ../../os/kernel/include/chheap.h ../../os/kernel/include/chmempools.h \
+ ../../os/kernel/include/chthreads.h ../../os/kernel/include/chdynamic.h \
+ ../../os/kernel/include/chregistry.h ../../os/kernel/include/chinline.h \
+ ../../os/kernel/include/chqueues.h ../../os/kernel/include/chstreams.h \
+ ../../os/kernel/include/chfiles.h ../../os/kernel/include/chdebug.h \
+ ../../os/hal/include/hal.h ../../boards/OLIMEX_STM32_E407/board.h \
+ halconf.h mcuconf.h ../../os/hal/platforms/STM32F4xx/hal_lld.h \
+ ../../os/hal/platforms/STM32/stm32.h \
+ ../../os/hal/platforms/STM32F4xx/stm32f4xx.h \
+ ../../os/ports/common/ARMCMx/CMSIS/include/core_cm4.h \
+ ../../os/ports/common/ARMCMx/CMSIS/include/core_cmInstr.h \
+ ../../os/ports/common/ARMCMx/CMSIS/include/core_cmFunc.h \
+ ../../os/ports/common/ARMCMx/CMSIS/include/core_cm4_simd.h \
+ ../../os/hal/platforms/STM32F4xx/stm32_isr.h \
+ ../../os/hal/platforms/STM32F4xx/stm32_dma.h \
+ ../../os/hal/platforms/STM32F4xx/stm32_rcc.h \
+ ../../os/hal/include/io_channel.h ../../os/hal/include/io_block.h \
+ ../../os/hal/include/mmcsd.h ../../os/hal/include/tm.h \
+ ../../os/hal/include/pal.h ../../os/hal/platforms/STM32/GPIOv2/pal_lld.h \
+ ../../os/hal/include/adc.h ../../os/hal/include/can.h \
+ ../../os/hal/include/ext.h ../../os/hal/include/gpt.h \
+ ../../os/hal/include/i2c.h ../../os/hal/include/icu.h \
+ ../../os/hal/include/mac.h ../../os/hal/platforms/STM32/mac_lld.h \
+ ../../os/hal/include/pwm.h ../../os/hal/include/rtc.h \
+ ../../os/hal/include/serial.h \
+ ../../os/hal/platforms/STM32/USARTv1/serial_lld.h \
+ ../../os/hal/include/sdc.h ../../os/hal/platforms/STM32/sdc_lld.h \
+ ../../os/hal/include/spi.h ../../os/hal/include/uart.h \
+ ../../os/hal/include/usb.h ../../os/hal/platforms/STM32/OTGv1/usb_lld.h \
+ ../../os/hal/platforms/STM32/OTGv1/stm32_otg.h \
+ ../../os/hal/include/mmc_spi.h ../../os/hal/include/serial_usb.h \
+ ../../os/hal/include/usb.h
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/string.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/_ansi.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/newlib.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/config.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/machine/ieeefp.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/features.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/reent.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/_ansi.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/_types.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/machine/_types.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/machine/_default_types.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/lock.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/include/stddef.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/cdefs.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/sys/string.h:
+
+../../os/kernel/include/ch.h:
+
+chconf.h:
+
+../../os/ports/GCC/ARMCMx/chtypes.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/include/stdint.h:
+
+/opt/psas/x-tools/stm32f407/arm-psas-eabi/bin/../lib/gcc/arm-psas-eabi/4.7.2/../../../../arm-psas-eabi/include/stdint.h:
+
+../../os/kernel/include/chlists.h:
+
+../../os/ports/GCC/ARMCMx/chcore.h:
+
+../../os/ports/GCC/ARMCMx/STM32F4xx/cmparams.h:
+
+../../os/ports/GCC/ARMCMx/chcore_v7m.h:
+
+../../os/ports/common/ARMCMx/nvic.h:
+
+../../os/kernel/include/chsys.h:
+
+../../os/kernel/include/chvt.h:
+
+../../os/kernel/include/chschd.h:
+
+../../os/kernel/include/chsem.h:
+
+../../os/kernel/include/chbsem.h:
+
+../../os/kernel/include/chmtx.h:
+
+../../os/kernel/include/chcond.h:
+
+../../os/kernel/include/chevents.h:
+
+../../os/kernel/include/chmsg.h:
+
+../../os/kernel/include/chmboxes.h:
+
+../../os/kernel/include/chmemcore.h:
+
+../../os/kernel/include/chheap.h:
+
+../../os/kernel/include/chmempools.h:
+
+../../os/kernel/include/chthreads.h:
+
+../../os/kernel/include/chdynamic.h:
+
+../../os/kernel/include/chregistry.h:
+
+../../os/kernel/include/chinline.h:
+
+../../os/kernel/include/chqueues.h:
+
+../../os/kernel/include/chstreams.h:
+
+../../os/kernel/include/chfiles.h:
+
+../../os/kernel/include/chdebug.h:
+
+../../os/hal/include/hal.h:
+
+../../boards/OLIMEX_STM32_E407/board.h:
+
+halconf.h:
+
+mcuconf.h:
+
+../../os/hal/platforms/STM32F4xx/hal_lld.h:
+
+../../os/hal/platforms/STM32/stm32.h:
+
+../../os/hal/platforms/STM32F4xx/stm32f4xx.h:
+
+../../os/ports/common/ARMCMx/CMSIS/include/core_cm4.h:
+
+../../os/ports/common/ARMCMx/CMSIS/include/core_cmInstr.h:
+
+../../os/ports/common/ARMCMx/CMSIS/include/core_cmFunc.h:
+
+../../os/ports/common/ARMCMx/CMSIS/include/core_cm4_simd.h:
+
+../../os/hal/platforms/STM32F4xx/stm32_isr.h:
+
+../../os/hal/platforms/STM32F4xx/stm32_dma.h:
+
+../../os/hal/platforms/STM32F4xx/stm32_rcc.h:
+
+../../os/hal/include/io_channel.h:
+
+../../os/hal/include/io_block.h:
+
+../../os/hal/include/mmcsd.h:
+
+../../os/hal/include/tm.h:
+
+../../os/hal/include/pal.h:
+
+../../os/hal/platforms/STM32/GPIOv2/pal_lld.h:
+
+../../os/hal/include/adc.h:
+
+../../os/hal/include/can.h:
+
+../../os/hal/include/ext.h:
+
+../../os/hal/include/gpt.h:
+
+../../os/hal/include/i2c.h:
+
+../../os/hal/include/icu.h:
+
+../../os/hal/include/mac.h:
+
+../../os/hal/platforms/STM32/mac_lld.h:
+
+../../os/hal/include/pwm.h:
+
+../../os/hal/include/rtc.h:
+
+../../os/hal/include/serial.h:
+
+../../os/hal/platforms/STM32/USARTv1/serial_lld.h:
+
+../../os/hal/include/sdc.h:
+
+../../os/hal/platforms/STM32/sdc_lld.h:
+
+../../os/hal/include/spi.h:
+
+../../os/hal/include/uart.h:
+
+../../os/hal/include/usb.h:
+
+../../os/hal/platforms/STM32/OTGv1/usb_lld.h:
+
+../../os/hal/platforms/STM32/OTGv1/stm32_otg.h:
+
+../../os/hal/include/mmc_spi.h:
+
+../../os/hal/include/serial_usb.h:
+
+../../os/hal/include/usb.h:
