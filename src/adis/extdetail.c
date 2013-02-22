353d40142c9f57c66d3f83280d4735ba0caa3dad
kwilson <kwilson@corvine.net>
2013-02-21 16:33:31 -0800

    Added second interrupt on PD9 for DIO1 from ADIS. This is the data ready SPI interrupt. (adis development).

--- src/adis/extdetail.c
+++ src/adis/extdetail.c
@@ -13,7 +13,7 @@
  */
 const EXTConfig extcfg = {
 		{
-				{EXT_CH_MODE_BOTH_EDGES | EXT_CH_MODE_AUTOSTART | EXT_MODE_GPIOA, extcb1},   // WKUP Button PA0
+				{EXT_CH_MODE_BOTH_EDGES | EXT_CH_MODE_AUTOSTART | EXT_MODE_GPIOA, extcb_wkup_btn},   // WKUP Button PA0
 				{EXT_CH_MODE_DISABLED, NULL},
 				{EXT_CH_MODE_DISABLED, NULL},
 				{EXT_CH_MODE_DISABLED, NULL},
@@ -22,7 +22,7 @@ const EXTConfig extcfg = {
 				{EXT_CH_MODE_DISABLED, NULL},
 				{EXT_CH_MODE_DISABLED, NULL},
 				{EXT_CH_MODE_DISABLED, NULL},
-				{EXT_CH_MODE_DISABLED, NULL},
+				{EXT_CH_MODE_FALLING_EDGE | EXT_CH_MODE_AUTOSTART | EXT_MODE_GPIOD, extcb_adis_dio1},
 				{EXT_CH_MODE_DISABLED, NULL},
 				{EXT_CH_MODE_DISABLED, NULL},
 				{EXT_CH_MODE_DISABLED, NULL},
@@ -48,7 +48,7 @@ void green_led_off(void *arg) {
 /* Triggered when the WKUP button is pressed or released. The LED is set to ON.*/
 
 /* Challenge: Add de-bouncing */
-void extcb1(EXTDriver *extp, expchannel_t channel) {
+void extcb_wkup_btn(EXTDriver *extp, expchannel_t channel) {
 	static VirtualTimer vt4;
 
 	(void)extp;
@@ -57,7 +57,6 @@ void extcb1(EXTDriver *extp, expchannel_t channel) {
 	palClearPad(GPIOC, GPIOC_LED);
 	chSysLockFromIsr();
 	chEvtBroadcastI(&wkup_event);
-	chEvtBroadcastI(&spi1_event);
 
 	if (chVTIsArmedI(&vt4))
 		chVTResetI(&vt4);
@@ -66,6 +65,17 @@ void extcb1(EXTDriver *extp, expchannel_t channel) {
 	chVTSetI(&vt4, MS2ST(500), green_led_off, NULL);
 	chSysUnlockFromIsr();
 }
+
+void extcb_adis_dio1(EXTDriver *extp, expchannel_t channel) {
+	(void)extp;
+	(void)channel;
+
+	chSysLockFromIsr();
+	chEvtBroadcastI(&spi1_event);
+
+	chSysUnlockFromIsr();
+}
+
 
 //! @}
 
