3532bec695705a736348dec936ff71d07e5a7125
kwilson <kwilson@corvine.net>
2013-03-17 18:59:04 -0700

    working on duplex ethernet transmissions

--- src/enet_udp/host_fc/eth_udp_fc.c
+++ src/enet_udp/host_fc/eth_udp_fc.c
@@ -15,6 +15,7 @@
 #define         BUFLEN          512
 #define         NPACK           100
 #define         PORT            35000
+#define         OUTPORT         35003
 
 void die_nice(char *s) {
     perror(s);
@@ -50,32 +51,3 @@ int main(void) {
     return 0;
 }
 
-#define SRV_IP "192.168.1.20"
-/* diep(), #includes and #defines like in the server */
-
-int main(void) {
-  struct sockaddr_in si_other;
-  int s, i, slen=sizeof(si_other);
-  char buf[BUFLEN];
-
-  if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1)
-    diep("socket");
-
-  memset((char *) &si_other, 0, sizeof(si_other));
-  si_other.sin_family = AF_INET;
-  si_other.sin_port = htons(PORT);
-  if (inet_aton(SRV_IP, &si_other.sin_addr)==0) {
-    fprintf(stderr, "inet_aton() failed\n");
-    exit(1);
-  }
-
-  for (i=0; i<NPACK; i++) {
-    printf("Sending packet %d\n", i);
-    sprintf(buf, "This is packet %d\n", i);
-    if (sendto(s, buf, BUFLEN, 0, &si_other, slen)==-1)
-      diep("sendto()");
-  }
-
-  close(s);
-  return 0;
-}
