18e25c4cda62638137ce9d15ff3a680aa73bb1c5
kwilson <kwilson@corvine.net>
2013-04-02 18:11:23 -0700

    mpu imu branch started

---
+++ src/mpu/extdetail.h
@@ -0,0 +1,38 @@
+/*! \file extdetail.h
+ *
+ */
+
+#ifndef _EXTDETAIL_H
+#define _EXTDETAIL_H
+
+/*!
+ * \addtogroup extdetail
+ * @{
+ */
+#include "ch.h"
+#include "hal.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern           EventSource     extdetail_wkup_event;
+
+extern const     EXTConfig       extcfg;
+
+void extdetail_WKUP_button_handler(eventid_t id) ;
+void extdetail_init(void) ;
+void extdetail_wkup_btn(EXTDriver *extp, expchannel_t channel) ;
+void extdetail_adis_dio1(EXTDriver *extp, expchannel_t channel) ;
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+//! @}
+
+
+#endif
+
