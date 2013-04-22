ee6876a1721a9189eed405c1d3d51686d19de469
David Ramage <dramage@comcast.net>
2013-04-22 10:14:59 -0700

    Adding headers

--- devices/include/MPU9150.h
+++ devices/include/MPU9150.h
@@ -187,6 +187,57 @@ typedef enum {
     A_G_WHO_AM_I           = 0x75     // RESET VAL: 0x68
 } mpu9150_a_g_regaddr;
 
+
+//type for MPL3115A2 registers
+
+typedev enum  {
+	STATUS 						= 0x00,
+	OUT_P_MSB					= 0x01,
+	OUT_P_CSB					= 0x02,
+	OUT_P_LSB					= 0x03,
+	OUT_T_MSB					= 0x04,
+	OUT_T_LSB					= 0x05,
+	DR_STATUS					= 0x06,
+	OUT_P_DELTA MSB			= 0x07,
+	OUT_P_DELTA_CSB			= 0x08,
+	OUT_P_DELTA_LSB			= 0x09,
+	OUT_T_DELTA_MSB			= 0x0A,
+	OUT_T_DELTA_LSB			= 0x0B,
+	WHO_AM_I						= 0x0C, //this is the MPL3115A2's address
+	F_STATUS						= 0x0D,
+	F_DATA						= 0x0E,
+	F_SETUP						= 0x0F,
+	TIME_DLY						= 0x10,
+	SYSMOD						= 0x11,
+	INT_SOURCE					= 0x12,
+	PT_DATA_CFG					= 0x13,
+	BAR_IN_MSB					= 0x14,
+	BAR_IN_LSB					= 0x15,
+	P_TGT_MSB					= 0x16,
+	P_TGT_LSB					= 0x17,
+	T_TGT							= 0x18,
+	P_WND_MSB					= 0x19,
+	P_WND_LSB					= 0x1A,
+	T_WND							= 0x1B,
+	P_MIN_MSB					= 0x1C,
+	P_MIN_CSB					= 0x1D,
+	P_MIN_LSB					= 0x1E,
+	T_MIN_MSB					= 0x1F,
+	T_MIN_LSB					= 0x20,
+	P_MAX_MSB					= 0x21,
+	P_MAX_CSB					= 0x22,
+	P_MAX_LSB					= 0x23,
+	T_MAX_MSB					= 0x24,
+	T_MAX_LSB					= 0x25,
+	CTRL_REG1					= 0x26,
+	CTRL_REG2					= 0x27,
+	CTRL_REG3					= 0x28,
+	CTRL_REG4					= 0x29,
+	CTRL_REG5					= 0x2A,
+	OFF_P							= 0x2B,
+	OFF_T							= 0x2C,
+	OFF_H							= 0x2D
+} mpl3115a2_regaddr;
 /*! \typedef mpu9150_a_g_burst_data
  * Burst data collection
  *
