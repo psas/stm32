502b931398c2ac83a68b69af47254995eff4d267
kwilson <kwilson@corvine.net>
2013-04-27 14:30:56 -0700

    added host_fc to mpu directory

---
+++ src/mpu/host_fc/mpu_fc.h
@@ -0,0 +1,34 @@
+/*! \file datap_fc.h
+ */
+
+#ifndef _DATAP_FC_H
+#define _DATAP_FC_H
+
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#include "fc_net.h"
+
+#define         MAXBUFLEN               512
+#define         NETBUFLEN               512
+#define         NPACK                   1000
+#define         PORT_STRING_LEN         6
+
+typedef enum thd_index {
+	SENSOR_LISTENER,
+	CONTROL_LISTENER
+} Thdindex;
+
+typedef struct Ports {
+	unsigned int       thread_id;
+	char               host_listen_port[PORT_STRING_LEN];
+	char               client_addr[INET6_ADDRSTRLEN];
+	char               client_port[PORT_STRING_LEN];
+} Ports;
+
+
+#endif
+
+
+
