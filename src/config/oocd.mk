535993b13430b841b5268214ca77df08d19fd7f4
TheoH <Theo0x48@gmail.com>
2013-06-04 22:29:38 -0700

    Made previous patch actually do something

--- src/config/oocd.mk
+++ src/config/oocd.mk
@@ -6,7 +6,7 @@ HEXFILE = $(BUILDDIR)/$(PROJECT).hex
 write: $(HEXFILE) write_ocd
 
 write_base:
-	export PATH=$(OPENOCD_PATH):$(PATH); sudo openocd -s $(OPENOCD) -f $(OOCD_CFG) -c "script oocd_prep.script" -c "flash write_image erase $(HEXFILE)" -c "reset" -c "shutdown"
+	sudo env PATH="$(OPENOCD_PATH):$(PATH)" openocd -s $(OPENOCD) -f $(OOCD_CFG) -c "script oocd_prep.script" -c "flash write_image erase $(HEXFILE)" -c "reset" -c "shutdown"
 
 write_ocd: OOCD_CFG = olimex_stm32_e407.cfg
 write_ocd: write_base
