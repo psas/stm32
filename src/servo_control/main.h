767b96d4162e2485138a9fc892e429d9f7d2087d
David Ramage <dramage@comcast.net>
2013-06-18 16:40:59 -0700

    getting all the directories

---
+++ src/servo_control/main.h
@@ -0,0 +1,46 @@
+/*! \file main.h
+ *
+ */
+
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/*! \mainpage adis ADIS IMU over Ethernet UDP experiments
+*
+*
+* \section intro Introduction
+*
+* Further developing the experiment started with adis, this code
+* adds processing to package and deliver a UDP packet of ADIS output
+* to a host computer running a Linux based OS.h
+*
+* \section filestructure File Tree
+*
+* <hr>
+*
+* @section notes release.notes
+* release.notes
+* <hr>
+* @section requirements Requirements
+*
+* @section copyright Copyright Notes
+*
+*** Notes **
+*
+*   Some files used by the demo are not part of ChibiOS/RT but are copyright of
+*   ST Microelectronics and are licensed under a different license.
+*   Also note that not all the files present in the ST library are distributed
+*   with ChibiOS/RT, you can find the whole library on the ST web site:
+*
+*                                http://www.st.com
+*
+* arm-psas-eabi-gcc compiler
+* ChibiOS source
+* Olimex stm32-e407 development board
+*
+* <hr>
+*
+*/
+
+#endif
+
