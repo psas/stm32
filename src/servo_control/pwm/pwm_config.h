27c195007dd1e2db76581f65a83b0006f023806f
kwilson <kwilsonpdx@gmail.com>
2013-06-24 15:55:10 -0700

    Testing udp PWM on 20mS (50hZ) servos. Added notes to pwm_config.h in servo_control about settings

--- src/servo_control/pwm/pwm_config.h
+++ src/servo_control/pwm/pwm_config.h
@@ -23,9 +23,35 @@
 
 #include "pwm_lld.h"
 
+
+/*! \warning PERIOD setting for PWM is uint16_t (0-65535)
+ *
+ *    Do not overflow this value! You may have to turn down
+ *    INIT_PWM_FREQ to obtain pulsewidths you need. See Hitec
+ *    20mS example settings.
+ * Testing only:
+ * Hitec 20mS period (50hZ) servos (Models HS-81 and HS-45HB)
+ *  Pulse widths range from 900 uS to 2100 uS
+
+#define         INIT_PWM_FREQ                  3000000
+#define         INIT_PWM_PERIOD_TICS           ((pwmcnt_t)  60000)
+#define         INIT_PWM_PULSE_WIDTH_TICS      ((pwmcnt_t)  4500)
+*/
+
+/* Rocket servo
+   3.3mS period (300hZ)
+   JS DS8717 Servo
+   http://www.servodatabase.com/servo/jr/ds8717
+
+    Pulse widths range from 1000 uS to 2000 uS
+        Hard stops on mechanism define this range. Center 1500uS pw.
+
+ */
+
 #define         INIT_PWM_FREQ                  6000000
 #define         INIT_PWM_PERIOD_TICS           ((pwmcnt_t) 20000)
 #define         INIT_PWM_PULSE_WIDTH_TICS      ((pwmcnt_t) 9000)
+
 
 #ifdef __cplusplus
 extern "C" {
