cda83eab81f8f7140ac677809bcf6189d9f955ac
Jenner Hanni <jeh.wicker@gmail.com>
2013-07-27 15:00:35 -0700

    Sends contents of a string out over serial using PA9 for USART1 TX.

--- src/gps/main.c
+++ src/gps/main.c
@@ -30,7 +30,6 @@ extern SerialDriver SD1; // defined in os/hal/platforms/STM32/USARTv1/serial_lld
 
 #define SERIAL_BITRATE 9600
 
-
 static const SerialConfig serial_config =
 {
 		SERIAL_BITRATE,
@@ -39,98 +38,36 @@ static const SerialConfig serial_config =
 		0
 };
 
-/*
- * A simple print a string function when you don't have printf...
- */
-static void my_println(char *p) {
-	while (*p) {
-		chSequentialStreamPut(&SD1, *p++);
-	}
-	// chSequentialStreamWrite(&SD1, (uint8_t *)"\n", 2);
-}
-
-static void lcd_scroll_right(void) {
-	chSequentialStreamPut(&SD1, 0xfe);
-	chSequentialStreamPut(&SD1, 0x1c);
-	chThdSleepMilliseconds(750);
-
-}
-
-static void lcd_clear_screen(void) {
-	chSequentialStreamPut(&SD1, 0xfe);
-	chSequentialStreamPut(&SD1, 0x01);
-}
 /* Simple Serial example
- * Write some chars out the tx port on the e407 board.
+ * Write some chars out the tx port on the FTDI output of the GPS RF board.
  *
- * CON3 pin D1 is TX. Also known as GPIO Port B, pin 6
+ * GPIO Port A pin 9 is our TX
  */
 int main(void) {
-	uint32_t    i          = 0;
+    uint32_t i = 0;
 
     halInit();
     chSysInit();
 
-    char	my_string[59] = "I am a good     gadget.";
-
-    /*
-     * Don't try this at home!
-     *
-     * This makes chprintf work with SerialDriver Type.
-     * This is an example of TYPE ABUSE and should be looked at very closely.
-     *
-     * See io_channel.h : _base_channel_methods
-     * See chstreams.h  : _base_sequential_stream_methods
-     *
-     * It's just luck. If this were real C++, you'd get a compile error. (We hope).
-     *
-     * ...But tracing why this is bad is a good way to learn about how Chibios is put
-     *    together.
-     */
-  //  BaseSequentialStream* chp = (BaseSequentialStream *)&SD1;
+    uint8_t my_string[10] = "Hello";
 
     /* Alternate pad mode is 7 for this port / pin.
      *
      * See the datasheet-NOT the reference manual
      */
-    palSetPadMode(GPIOB, GPIOB_PIN6, PAL_MODE_ALTERNATE(7));
-    palSetPadMode(GPIOB, GPIOB_PIN7, PAL_MODE_ALTERNATE(7));
+      palSetPadMode(GPIOA, 10, PAL_MODE_ALTERNATE(7));  // RX
+      palSetPadMode(GPIOA, 9, PAL_MODE_ALTERNATE(7));   // TX
 
     /* activate serial driver 1
      * default configuration ('NULL' means use default.)
      */
-    //sdStart(&SD1, &serial_config); // default setup is 38400 8N1 for Chibios
+    sdStart(&SD1, &serial_config); // default setup is 38400 8N1 for Chibios
 
     while (TRUE) {
-    	palTogglePad(GPIOA, GPIOA_PIN11);
-    	chThdSleepMilliseconds(500);
-#if 0
-        //chThdSleepMilliseconds(5);
-        for( i = 0; i < 1000000; i++ ) {
-
-        }
-
-
-        palClearPort(GPIOA, GPIOA_PIN11);
-//        chThdSleepMilliseconds(5);
-        for(i = 0; i < 1000000; i++ ) {
-
-                }
-        palSetPort(GPIOA, GPIOA_PIN11);
-#endif
+      sdWrite(&SD1,my_string,strlen(my_string));
+      sdWrite(&SD1,"\n\r",2);
+      sdWrite(&SD1,"Hello, world!\n\r",15);
+      chThdSleepMilliseconds(1000);
     }
-
-#if 0
-    while (TRUE) {
-    	chThdSleepMilliseconds(750);
-    	lcd_clear_screen();
-    	//chprintf(chp, "%s fn. ", __func__);
-    	my_println(my_string);
-    	chThdSleepMilliseconds(1500);
-    	for(i=0; i<16; ++i) {
-    		lcd_scroll_right();
-    	}
-    }
-#endif
 }
 
