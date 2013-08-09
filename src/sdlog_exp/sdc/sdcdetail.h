cbfbd30410a65184d7adb8ead051bc56f0dfb7ba
kwilson <kwilsonpdx@gmail.com>
2013-08-08 20:59:06 -0700

    Chasing strange card behavior on existing vs. non-exisiting file.

--- src/sdlog_exp/sdc/sdcdetail.h
+++ src/sdlog_exp/sdc/sdcdetail.h
@@ -17,13 +17,16 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-#define         SDLOG_THREAD_STACK_SIZE         512
+
+#define         SDLOG_THREAD_STACKSIZE_BYTES                  1024
+
 
 extern          bool        fs_ready;
 extern          FATFS       SDC_FS;
 
 extern          EventSource inserted_event;
 extern          EventSource removed_event;
+extern          WORKING_AREA(wa_sdlog_thread, SDLOG_THREAD_STACKSIZE_BYTES);
 
 typedef struct logdata {
    uint32_t     index;
@@ -35,6 +38,7 @@ void            RemoveHandler(eventid_t id) ;
 void            sdc_tmr_init(void *p) ;
 
 FRESULT         sdc_scan_files(BaseSequentialStream *chp, char *path) ;
+msg_t           sdlog_thread(void *p) ;
 
 #ifdef __cplusplus
 }
