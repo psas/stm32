f66c12bde564a3986e02e51095fcac6998c3b8ff
kwilson <kwilsonpdx@gmail.com>
2013-09-02 12:37:57 -0700

    Updates to e407 serial. Added data_udp threads.

--- src/e407_serial/main.c
+++ src/e407_serial/main.c
@@ -1,22 +1,22 @@
 /*
-    ChibiOS/RT - Copyright (C) 2006,2007,2008,2009,2010,
-                 2011,2012 Giovanni Di Sirio.
+   ChibiOS/RT - Copyright (C) 2006,2007,2008,2009,2010,
+   2011,2012 Giovanni Di Sirio.
 
-    This file is part of ChibiOS/RT.
+   This file is part of ChibiOS/RT.
 
-    ChibiOS/RT is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
-    (at your option) any later version.
+   ChibiOS/RT is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
 
-    ChibiOS/RT is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+   ChibiOS/RT is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   */
 #include <stdbool.h>
 #include <stdlib.h>
 #include "ch.h"
@@ -24,6 +24,19 @@
 #include "chprintf.h"
 #include "shell.h"
 #include "cmddetail.h"
+#include <lwip/ip_addr.h>
+#include "iwdg_lld.h"
+#include "data_udp.h"
+#include "lwipopts.h"
+#include "lwipthread.h"
+
+#include "board.h"
+
+#include "device_net.h"
+#include "fc_net.h"
+
+#include "main.h"
+
 
 /*! The goal of this code is to run the shell through the serial terminal
  * and not the usb subsystem. Connect an FTDI serial/usb connector to the
@@ -46,70 +59,103 @@
 static uint32_t           led_wait_time         =        500;
 
 static const ShellCommand commands[] = {
-		{"phy_read", cmd_phy_read},
-		{"find_phy", cmd_find_phy},
-		{"mem", cmd_mem},
-		{"threads", cmd_threads},
-		{NULL, NULL}
+    {"phy_read", cmd_phy_read},
+    {"find_phy", cmd_find_phy},
+    {"mem", cmd_mem},
+    {"threads", cmd_threads},
+    {NULL, NULL}
 };
 
 static const ShellConfig shell_cfg1 = {
-		(BaseSequentialStream *)&SD6,
-		commands
+    (BaseSequentialStream *)&SD6,
+    commands
 };
 
 static WORKING_AREA(waThread_blinker, 64);
 /*! \brief Green LED blinker thread
- */
+*/
 static msg_t Thread_blinker(void *arg) {
-	(void)arg;
-	chRegSetThreadName("blinker");
-	while (TRUE) {
-		palTogglePad(GPIOC, GPIOC_LED);
-		chThdSleepMilliseconds(led_wait_time);
-	}
-	return -1;
+    (void)arg;
+    chRegSetThreadName("blinker");
+    while (TRUE) {
+        palTogglePad(GPIOC, GPIOC_LED);
+        chThdSleepMilliseconds(led_wait_time);
+    }
+    return -1;
 }
 
+static WORKING_AREA(waThread_indwatchdog, 64);
+/*! \brief  Watchdog thread
+*/
+static msg_t Thread_indwatchdog(void *arg) {
+    (void)arg;
 
-/*
- * Application entry point.
- */
+    chRegSetThreadName("iwatchdog");
+    while (TRUE) {
+        iwdg_lld_reload();
+        chThdSleepMilliseconds(250);
+    }
+    return -1;
+}
+
 int main(void) {
-	static Thread            *shelltp       = NULL;
-	/*
-	 * System initializations.
-	 * - HAL initialization, this also initializes the configured device drivers
-	 *   and performs the board-specific initializations.
-	 * - Kernel initialization, the main() function becomes a thread and the
-	 *   RTOS is active.
-	 */
-	halInit();
-	chSysInit();
+    static Thread            *shelltp       = NULL;
 
-	// start the serial port
-	sdStart(&SD6, NULL);
+    struct lwipthread_opts   ip_opts;
 
-	/*
-	 * Shell manager initialization.
-	 */
-	shellInit();
-	chThdCreateStatic(waThread_blinker          , sizeof(waThread_blinker)          , NORMALPRIO    , Thread_blinker         , NULL);
-	/*
-	 * Normal main() thread activity, in this demo it enables and disables the
-	 * button EXT channel using 5 seconds intervals.
-	 */
+    /*
+     * System initializations.
+     * - HAL initialization, this also initializes the configured device drivers
+     *   and performs the board-specific initializations.
+     * - Kernel initialization, the main() function becomes a thread and the
+     *   RTOS is active.
+     */
+    halInit();
+    chSysInit();
 
-	while (true) {
-		if (!shelltp )
-			shelltp = shellCreate(&shell_cfg1, SHELL_WA_SIZE, NORMALPRIO);
-		else if (chThdTerminated(shelltp)) {
-			chThdRelease(shelltp);    /* Recovers memory of the previous shell.   */
-			shelltp = NULL;           /* Triggers spawning of a new shell.        */
-		}
-		chThdSleepMilliseconds(1000);
-	}
+    // start the serial port
+    sdStart(&SD6, NULL);
+    chThdSleepMilliseconds(300);
+    /*
+     * Shell manager initialization.
+     */
+    shellInit();
 
-	exit(0);
+    iwdg_begin();
+
+    chThdCreateStatic(waThread_blinker          , sizeof(waThread_blinker)          , NORMALPRIO    , Thread_blinker         , NULL);
+    chThdCreateStatic(waThread_indwatchdog      , sizeof(waThread_indwatchdog)      , NORMALPRIO    , Thread_indwatchdog     , NULL);
+
+    static       uint8_t      IMU_macAddress[6]           = IMU_A_MAC_ADDRESS;
+    struct       ip_addr      ip, gateway, netmask;
+    IMU_A_IP_ADDR(&ip);
+    IMU_A_GATEWAY(&gateway);
+    IMU_A_NETMASK(&netmask);
+
+    ip_opts.macaddress = IMU_macAddress;
+    ip_opts.address    = ip.addr;
+    ip_opts.netmask    = netmask.addr;
+    ip_opts.gateway    = gateway.addr;
+
+    chThdCreateStatic(wa_lwip_thread            , sizeof(wa_lwip_thread)            , NORMALPRIO + 2, lwip_thread            , &ip_opts);
+    chThdCreateStatic(wa_data_udp_send_thread   , sizeof(wa_data_udp_send_thread)   , NORMALPRIO    , data_udp_send_thread   , NULL);
+    chThdCreateStatic(wa_data_udp_receive_thread, sizeof(wa_data_udp_receive_thread), NORMALPRIO    , data_udp_receive_thread, NULL);
+
+    /*
+     * Normal main() thread activity, in this demo it enables and disables the
+     * button EXT channel using 5 seconds intervals.
+     */
+
+    while (true) {
+        if (!shelltp )
+            shelltp = shellCreate(&shell_cfg1, SHELL_WA_SIZE, NORMALPRIO);
+        else if (chThdTerminated(shelltp)) {
+            chThdRelease(shelltp);    /* Recovers memory of the previous shell.   */
+            shelltp = NULL;           /* Triggers spawning of a new shell.        */
+        }
+        chThdSleepMilliseconds(1000);
+    }
+
+    exit(0);
 }
 
