180696a91c46b828f307e957a2ea2818965ebabf
aperiodic <dlp@aperiodic.org>
2013-09-28 15:11:14 -0700

    Basic multithreaded mailbox test works

--- src/mailbox_test/main.c
+++ src/mailbox_test/main.c
@@ -23,28 +23,74 @@
 #include "ch.h"
 #include "hal.h"
 #include "chprintf.h"
-
 #include "usbdetail.h"
 
 #define MB_SIZE 32
+#define TEST_MB_MSG_LENGTH 13
 
 
 msg_t mail_buffer[MB_SIZE];
 
 static MAILBOX_DECL(my_mail, mail_buffer, MB_SIZE);
 
-void mail_singlethread_test(void) {
+static bool mail_singlethread_test(void) {
   chMBPost(&my_mail, 'A', TIME_IMMEDIATE);
   msg_t fetched_msg;
   msg_t fetch_status = chMBFetch(&my_mail, &fetched_msg, TIME_IMMEDIATE);
 
   if (fetch_status != RDY_OK) {
     // figure out how to print out an error here
+    return false;
   }
 
   if (fetched_msg != 'A') {
     // figure out how to print out an error here
+    return false;
   }
+
+  return true;
+}
+
+static char *TEST_MESSAGE = "HELLO WORLD!";
+static WORKING_AREA(waThread_Test_Sender, 64);
+
+static msg_t mail_multithread_test_sender(void *_) {
+  msg_t status;
+  int i;
+
+  for (i = 0; i < TEST_MB_MSG_LENGTH; i++) {
+    //chprintf((BaseSequentialStream *) &SDU_PSAS, "sending test message #%d\r\n", i);
+    status = chMBPost(&my_mail, TEST_MESSAGE[i], TIME_IMMEDIATE);
+    if (status != RDY_OK) {
+      //chprintf((BaseSequentialStream *) &SDU_PSAS, "could not send test message #%d\r\n", i);
+    }
+  }
+
+  return 0;
+}
+
+static msg_t mail_test_buffer[TEST_MB_MSG_LENGTH];
+static WORKING_AREA(waThread_Test_Receiver, 512);
+
+static msg_t mail_multithread_test_receiver(void *_) {
+  int i;
+
+  for (i = 0; i < TEST_MB_MSG_LENGTH; i++) {
+    chprintf((BaseSequentialStream *) &SDU_PSAS, "waiting for test message\r\n", i);
+    chMBFetch(&my_mail, &mail_test_buffer[i], TIME_INFINITE);
+  }
+
+  for (i = 0; i < TEST_MB_MSG_LENGTH; i++) {
+    if (mail_test_buffer[i] != TEST_MESSAGE[i]) {
+      chprintf((BaseSequentialStream *) &SDU_PSAS, "test message #%d is incorrect!\r\n", i);
+      chprintf((BaseSequentialStream *) &SDU_PSAS, "expecting #%c, got #%c\r\n", i);
+      return 1;
+    }
+  }
+
+  chprintf((BaseSequentialStream *) &SDU_PSAS, "all test messages received as expected!\r\n");
+
+  return 0;
 }
 
 /*
@@ -84,12 +130,30 @@ int main(void) {
 	 */
 	sdStart(&SD6, NULL);
 
-	chThdSleepMilliseconds(300);
+	chThdSleepMilliseconds(1300);
+
+
+  /*
+   * Spawn the test threads
+   */
+	chThdCreateStatic( waThread_Test_Receiver
+                   , sizeof(waThread_Test_Receiver)
+                   , NORMALPRIO
+                   , mail_multithread_test_receiver
+                   , NULL
+                   );
+	chThdCreateStatic( waThread_Test_Sender
+                   , sizeof(waThread_Test_Sender)
+                   , NORMALPRIO
+                   , mail_multithread_test_sender
+                   , NULL
+                   );
+
   /*
    * Normal main() thread activity,
    */
   while (1) {
-    chprintf((BaseSequentialStream *) &SDU_PSAS, "hello over USB Serial from the STM32!!\r\n");
+    //chprintf((BaseSequentialStream *) &SDU_PSAS, "hello over USB Serial from the STM32!!\r\n");
     chThdSleep(MS2ST(1000));
   }
   exit(0);
