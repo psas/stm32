fc3feb2ced72e3a8b78778a4a138e7e3f74e613d
TheoH <Theo0x48@gmail.com>
2013-10-04 18:04:51 -0700

    Fixed Makefiles
    If it built before it should build now
    If it didn't build before it should still have the same issues it did
    but should not be affected by my previous changes

--- common/oocd.mk
+++ common/oocd.mk
@@ -1,13 +1,13 @@
 
-OPENOCD = ../../openocd
+OPENOCD = $(PSAS_OPENOCD)
 OPENOCD_PATH = /opt/psas/openocd/bin
 OPENOCD_SEARCH = $(OPENOCD_PATH)/../share/openocd/scripts
-HEXFILE = $(BUILDDIR)/$(PROJECT).hex
+OPENOCD_HEXFILE = $(BUILDDIR)/$(PROJECT).hex
 
-write: $(HEXFILE) write_ocd
+write: $(OPENOCD_HEXFILE) write_ocd
 
 write_base:
-	sudo env PATH="$(OPENOCD_PATH):$(PATH)" openocd -s $(OPENOCD) -s $(OPENOCD_SEARCH) -f $(OOCD_CFG) -c "script oocd_prep.script" -c "flash write_image erase $(HEXFILE)" -c "reset" -c "shutdown"
+	sudo env PATH="$(OPENOCD_PATH):$(PATH)" openocd -s $(OPENOCD) -s $(OPENOCD_SEARCH) -f $(OOCD_CFG) -c "script oocd_prep.script" -c "flash write_image erase $(OPENOCD_HEXFILE)" -c "reset" -c "shutdown"
 
 write_ocd: OOCD_CFG = olimex_stm32_e407.cfg
 write_ocd: write_base
