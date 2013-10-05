e3bc5caebae2cca37cae64a447335aa44cffc4aa
kwilson <kwilsonpdx@gmail.com>
2013-10-04 17:34:26 -0700

    more sdlog experiments

--- src/sdlog_exp/sdc/sdcdetail.h
+++ src/sdlog_exp/sdc/sdcdetail.h
@@ -13,13 +13,11 @@
 #include "hal.h"
 #include "ff.h"
 
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #define         SDLOG_THREAD_STACKSIZE_BYTES                  1024
-
 
 extern          bool        fs_ready;
 extern          FATFS       SDC_FS;
@@ -27,6 +25,52 @@ extern          FATFS       SDC_FS;
 extern          EventSource inserted_event;
 extern          EventSource removed_event;
 extern          WORKING_AREA(wa_sdlog_thread, SDLOG_THREAD_STACKSIZE_BYTES);
+
+/*! Start of a list of codes for events
+ */
+typedef enum EVNTCode {
+        OK_EVENT  = 0,
+	PWR_RESET, 
+	WDG_RESET, 
+	ADIS_DATA, 
+	OTH_RESET, 
+	ADIS_INT, 
+	MPL_INT, 
+	MPU_INT, 
+	FCF_MSG 
+} EVNTCode;
+
+struct Message_head {
+    char                 ID[4];
+    uint8_t              timestamp[6];
+    uint16_t             data_length;
+} __attribute__((packed));
+typedef struct Message_head Message_head;
+
+struct ADIS_message {
+        Message_head         message_head;
+        ADIS16405_burst_data data;
+} __attribute__((packed));
+typedef struct ADIS_message ADIS_message;
+
+typedef struct Event_message {
+        Message_head         message_head;
+        int                  event;
+        uint8_t[10]          event_str;   // human readable note
+} __attribute__((packed));
+typedef struct EVNT_message EVNT_message;
+
+typedef struct MPU_message {
+        Message_head         message_head;
+        mpudata              dataevent;
+}__attribute__((packed));
+typedef struct MPU_message MPU_message;
+
+typedef struct MPL_message {
+        Message_head         message_head;
+        mpldata              data;
+}__attribute__((packed));
+typedef struct MPL_message MPL_message;
 
 typedef struct logdata {
    uint32_t     index;
@@ -44,10 +88,7 @@ msg_t           sdlog_thread(void *p) ;
 }
 #endif
 
-
-
 //! @}
-
 
 #endif
 
