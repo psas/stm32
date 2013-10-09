9800a7ca88bc605363b405257f8c2194fcc2a24f
TheoH <Theo0x48@gmail.com>
2013-10-09 03:21:10 -0700

    Re-fixed RNH board.mk

--- common/boards/PSAS_ROCKETNET_HUB_1_0/board.mk
+++ common/boards/PSAS_ROCKETNET_HUB_1_0/board.mk
@@ -1,6 +1,6 @@
-# List of all the board related files.
-BOARDSRC = ./boards/PSAS_ROCKETNET_HUB_1_0/board.c
-
 # Required include directories
-BOARDINC = ./boards/PSAS_ROCKETNET_HUB_1_0
+BOARDINC = $(PSAS_BOARDS)/PSAS_ROCKETNET_HUB_1_0
+
+# List of all the board related files.
+BOARDSRC = $(BOARDINC)/board.c
 
