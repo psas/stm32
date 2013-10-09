dbcf841a2d9ff79f9df2e49e608d19229423a243
TheoH <Theo0x48@gmail.com>
2013-10-08 19:54:04 -0700

    Removed redundant make rules

--- common/psas.mk
+++ common/psas.mk
@@ -4,11 +4,3 @@ PSAS_OPENOCD       = ../../toolchain/openocd
 PSAS_DEVICES       = $(PSAS_COMMON)/devices
 PSAS_NET           = $(PSAS_COMMON)/net
 
-%: $(OBJDIR) $(LSTDIR)
-
-$(OBJDIR):
-	mkdir -p $(OBJDIR)
-
-$(LSTDIR):
-	mkdir -p $(LSTDIR)
-
