56c17450ff1c6ab6d27b2a7b76dcc8983cca5e02
TheoH <Theo0x48@gmail.com>
2013-10-08 21:30:45 -0700

    Fixed common RNH board makefile

--- common/psas.mk
+++ common/psas.mk
@@ -3,4 +3,4 @@ PSAS_COMMON        = ../../common
 PSAS_OPENOCD       = ../../toolchain/openocd
 PSAS_DEVICES       = $(PSAS_COMMON)/devices
 PSAS_NET           = $(PSAS_COMMON)/net
-
+PSAS_BOARDS        = $(PSAS_COMMON)/boards
