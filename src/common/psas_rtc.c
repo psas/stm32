f389b5c2a2b2ebfbcefc0e312cd17fc110ccead0
kwilson <kwilsonpdx@gmail.com>
2013-10-16 13:31:23 -0700

    Functions to convert between psas timespec and RTCTime

--- src/common/psas_rtc.c
+++ src/common/psas_rtc.c
@@ -1,21 +1,21 @@
 /*! \file psas_rtc.c
  *
-    Some parts of this file under this license:
+ Some parts of this file under this license:
 
-    ChibiOS/RT - Copyright (C) 2006-2013 Giovanni Di Sirio
+ ChibiOS/RT - Copyright (C) 2006-2013 Giovanni Di Sirio
 
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-        http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
- */
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 /*!
  * \defgroup psas_rtc Non standard PSAS RTC
@@ -41,6 +41,7 @@
 #include <string.h>
 #include <stdbool.h>
 #include <time.h>
+#include <math.h>
 #include "ch.h"
 #include "hal.h"
 #include "rtc.h"
@@ -57,8 +58,8 @@
 #define psas_rtc_lld_apb1_sync() {while ((RTCD1.id_rtc->ISR & RTC_ISR_RSF) == 0);}
 
 #define psas_rtc_lld_enter_init() {                                           \
-  RTCD1.id_rtc->ISR |= RTC_ISR_INIT;                                          \
-  while ((RTCD1.id_rtc->ISR & RTC_ISR_INITF) == 0)                            \
+    RTCD1.id_rtc->ISR |= RTC_ISR_INIT;                                          \
+    while ((RTCD1.id_rtc->ISR & RTC_ISR_INITF) == 0)                            \
     ;                                                                         \
 }
 
@@ -112,7 +113,7 @@ void psas_rtc_lld_init(void) {
      * preset_s = (STM32_RTCCLK / (prediv_a + 1)) - 1)
      */
     prediv_a = (prediv_a << 16) |
-            (((STM32_RTCCLK / (prediv_a + 1)) - 1) & 0x7FFF);
+        (((STM32_RTCCLK / (prediv_a + 1)) - 1) & 0x7FFF);
     RTCD1.id_rtc->PRER = prediv_a;
     RTCD1.id_rtc->PRER = prediv_a;
 
@@ -159,36 +160,25 @@ void psas_stm32_rtc_bcd2tm(struct tm *timp, RTCTime *timespec) {
     timp->tm_hour += ((tv_time & RTC_TR_HT) >> RTC_TR_HT_OFFSET) * 10;
     timp->tm_hour += 12 * ((tv_time & RTC_TR_PM) >> RTC_TR_PM_OFFSET);
 }
-/* Mon 14 October 2013 18:17:57 (PDT) untested functions */
-/* Next step: Test these */
 
+/*! \brief Convert from an RTCTime struct to a psas_timespec */
 void psas_rtc_to_psas_ts(psas_timespec* ts, RTCTime* rtc) {
     uint64_t  total_ns         = 0;
-    uint32_t  total_ns_lower   = 0;
-    uint32_t  total_ns_upper   = 0;
-    
-    total_ns              = rtc->tv_date    * 1e9u;
-    total_ns              += (rtc->tv_msec) * 1e6u;
 
-    total_ns_lower        = total_ns   & 0xFFFFFFFF;
-    total_ns_upper        = ((total_ns & (0xFFFF << 32)) >> 32);
-
+    total_ns              = rtc->tv_time    * 1e9;
+    total_ns              += (rtc->tv_msec) * 1e6;
     // memcpy(dst, src, length)
-    memcpy(ts->PSAS_ns[0], &total_ns_lower, sizeof(uint32_t));
-    memcpy(ts->PSAS_ns[4], &total_ns_upper, sizeof(uint32_t));
+    memcpy(&ts->PSAS_ns[0], &total_ns, 6);
 }
 
+/*! \brief Convert from a psas_timespec to an RTCTime struct */
 void psas_ts_to_psas_rtc(RTCTime* rtc, psas_timespec* ts) {
+    uint64_t   total_ns_in    = 0;
     uint64_t   total_ns       = 0;
-    uint64_t   total_ns_upper = 0;
-    uint32_t   total_ns_lower = 0;
 
-    total_ns_lower = (ts->PSAS_ns[3] << 24) | (ts->PSAS_ns[2] << 16) |(ts->PSAS_ns[1] << 8) | (ts->PSAS_ns[0] )
-    total_ns_upper = (ts->PSAS_ns[5] << 8)  | ts->PSAS_ns[4];
-    total_ns       = total_ns_upper  << 32;
-    total_ns       = total_ns + total_ns_lower;
-    rtc->tv_date   = (uint32_t) (floor(total_ns / 1e9u));
-    rtc->tv_msec   = (uint32_t) ((total_ns - (rtc->tv_date * 1e9u))/1e6u);
+    memcpy(&total_ns_in, &ts->PSAS_ns[0], 6);
+    rtc->tv_time   = (uint32_t) (floor(total_ns_in / 1e9));
+    rtc->tv_msec   = (uint32_t) ((total_ns - total_ns_in)/1e6);
 }
 
 /*!
@@ -215,5 +205,21 @@ void psas_rtc_lld_get_time( RTCDriver *rtcp, RTCTime *timespec) {
 
 }
 
+/*!
+ * \brief set current time
+ */
+void psas_rtc_lld_set_time( RTCDriver *rtcp, RTCTime *timespec) {
+    double ssr_ticks_d;
+
+    /* set seconds */
+    rtc_lld_set_time(rtcp, (const RTCTime *) timespec) ;
+
+    /* set milliseconds
+     * 32768 crystal scaled at pre_a=1 is ~60uS per tick. 
+     *  ... this is 16.384mS per tick 
+     */
+    ssr_ticks_d       = 16.384 * timespec->tv_msec;
+    RTCD1.id_rtc->SSR = ceil(ssr_ticks_d);
+}
 
 //! @}
