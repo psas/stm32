a55833d8f004fefa8db71d89b33e0d1bbd844f77
kwilson <kwilsonpdx@gmail.com>
2013-11-11 19:32:46 -0800

    Taking apart sdlog thread.

--- projects/flight-imu/sdc/sdcdetail.c
+++ projects/flight-imu/sdc/sdcdetail.c
@@ -41,6 +41,18 @@ BaseSequentialStream    *chp   =  (BaseSequentialStream *)&SDU_PSAS;
 static const    char*           sdc_log_data_file                = "LOGSMALL.bin";
 static const    unsigned        sdlog_thread_sleeptime_ms        = 1234;
 
+static struct dfstate {
+    uint32_t            log_sequence;  
+    uint32_t            write_errors;
+
+    GENERIC_message     log_data;
+
+    FIL                 DATAFil;
+
+    bool                sd_log_opened;
+} datafile_state;
+
+
 /*! Stack area for the sdlog_thread.  */
 WORKING_AREA(wa_sdlog_thread, SDC_THREAD_STACKSIZE_BYTES); 
 
@@ -51,16 +63,6 @@ WORKING_AREA(wa_sdlog_thread, SDC_THREAD_STACKSIZE_BYTES);
  */
 msg_t sdlog_thread(void *p) {
     void * arg __attribute__ ((unused)) = p;
-
-    uint32_t            log_index      = 0;
-    uint32_t            write_errors   = 0;
-
-    GENERIC_message     log_data;
-
-    FIL                 DATAFil;
-
-    bool                sd_log_opened  = false;
-
     chRegSetThreadName("sdlog_thread");
 
 #ifdef DEBUG_SDLOGEXP
@@ -68,6 +70,11 @@ msg_t sdlog_thread(void *p) {
 #endif
 
     SDLOGEXPDBG("Start sdlog thread\r\n");
+
+    // init structure
+    datafile_state.log_sequence  = 0;
+    datafile_state.write_errors  = 0;
+    datafile_state.sd_log_opened = false;
 
     sdc_reset_fp_index();
 
@@ -80,54 +87,55 @@ msg_t sdlog_thread(void *p) {
     }
 
     // Assert we will not overflow Payload
-    if(sizeof(MPU9150_read_data) > (sizeof(log_data.data)-1)) {
+    if(sizeof(MPU9150_read_data) > (sizeof(datafile_state.log_data.data)-1)) {
         SDLOGEXPDBG("%s: DATA size is too large\r\n");
         return (SDC_ASSERT_ERROR);
     }
 
+    Mon 11 November 2013 19:30:48 (PST) break this out for separate function now.
     while(1) {
         uint32_t            bw;
         int                 rc;
         FRESULT             ret;
 
-        if(fs_ready && !sd_log_opened ) {
+        if(fs_ready && !datafile_state.sd_log_opened ) {
             // open an existing log file for writing
-            ret = f_open(&DATAFil, sdc_log_data_file, FA_OPEN_EXISTING | FA_WRITE );
+            ret = f_open(&datafile_state.DATAFil, sdc_log_data_file, FA_OPEN_EXISTING | FA_WRITE );
             if(ret) { // try again....
                 SDLOGEXPDBG("open existing ret: %d\r\n", ret);
-                ret = f_open(&DATAFil, sdc_log_data_file, FA_OPEN_EXISTING | FA_WRITE );
+                ret = f_open(&datafile_state.DATAFil, sdc_log_data_file, FA_OPEN_EXISTING | FA_WRITE );
             }
 
             if (ret) {
                 SDLOGEXPDBG("failed to open existing %s\r\n",sdc_log_data_file);
                 // ok...try creating the file
-                ret = f_open(&DATAFil, sdc_log_data_file, FA_CREATE_ALWAYS | FA_WRITE   );
+                ret = f_open(&datafile_state.DATAFil, sdc_log_data_file, FA_CREATE_ALWAYS | FA_WRITE   );
                 if(ret) {
                     // try again 
                     SDLOGEXPDBG("open new file ret: %d\r\n", ret);
-                    ret = f_open(&DATAFil, sdc_log_data_file, FA_CREATE_ALWAYS | FA_WRITE   );
+                    ret = f_open(&datafile_state.DATAFil, sdc_log_data_file, FA_CREATE_ALWAYS | FA_WRITE   );
                 }
                 if (ret) {
-                    sd_log_opened = false;
+                    datafile_state.sd_log_opened = false;
                 } else {
-                    sd_log_opened = true;
+                    datafile_state.sd_log_opened = true;
                 }
             } else {
                 SDLOGEXPDBG("Opened existing file OK.\r\n");
-                sd_log_opened = true;
-                write_errors  = 0;
+                datafile_state.sd_log_opened = true;
+                datafile_state.write_errors  = 0;
             }
         }
 
-        if (fs_ready && sd_log_opened) {
+        if (fs_ready && datafile_state.sd_log_opened) {
             crc_t          crc16;
             RTCTime        timenow;
 
             // ID of this message
-            strncpy(log_data.mh.ID, mpuid, sizeof(log_data.mh.ID));   // pretend to use mpu sensor for testing
+            strncpy(datafile_state.log_data.mh.ID, mpuid, sizeof(datafile_state.log_data.mh.ID));   // pretend to use mpu sensor for testing
 
             // index
-            log_data.mh.index       = log_index++;
+            datafile_state.log_data.mh.index       = datafile_state.log_sequence++;
 
             // timestamp
             timenow.h12             = 1;
@@ -136,30 +144,30 @@ msg_t sdlog_thread(void *p) {
             if (rc == -1) {
                 SDLOGEXPDBG( "%s: psas_rtc time read errors: %d\r\n",__func__, rc);
             }
-            log_data.logtime.tv_time = timenow.tv_time;
-            log_data.logtime.tv_msec = timenow.tv_msec;
-            psas_rtc_to_psas_ts(&log_data.mh.ts, &timenow);
+            datafile_state.log_data.logtime.tv_time = timenow.tv_time;
+            datafile_state.log_data.logtime.tv_msec = timenow.tv_msec;
+            psas_rtc_to_psas_ts(&datafile_state.log_data.mh.ts, &timenow);
 
-            memcpy(&log_data.data, (void*) &mpu9150_current_read, sizeof(MPU9150_read_data) );
+            memcpy(&datafile_state.log_data.data, (void*) &mpu9150_current_read, sizeof(MPU9150_read_data) );
 
-            log_data.mh.data_length = sizeof(MPU9150_read_data);
+            datafile_state.log_data.mh.data_length = sizeof(MPU9150_read_data);
 
-            rc = sdc_write_log_message(&DATAFil, &log_data, &bw) ;
-            if(rc != FR_OK ) { ++write_errors; SDLOGEXPDBG("*"); }
+            rc = sdc_write_log_message(&datafile_state.DATAFil, &datafile_state.log_data, &bw) ;
+            if(rc != FR_OK ) { ++datafile_state.write_errors; SDLOGEXPDBG("*"); }
 
             // calc checksum
             crc16                   = crc_init();
-            crc16                   = crc_update(crc16, (const unsigned char*) &log_data, sizeof(GENERIC_message));
+            crc16                   = crc_update(crc16, (const unsigned char*) &datafile_state.log_data, sizeof(GENERIC_message));
             crc16                   = crc_finalize(crc16);
 
-            rc = sdc_write_checksum(&DATAFil, &crc16, &bw) ;
+            rc = sdc_write_checksum(&datafile_state.DATAFil, &crc16, &bw) ;
 
-            if(rc != FR_OK ) { ++write_errors; SDLOGEXPDBG("+"); }
+            if(rc != FR_OK ) { ++datafile_state.write_errors; SDLOGEXPDBG("+"); }
 
 #ifdef DEBUG_SDLOGEXP
             if((sdc_fp_index - sdc_fp_index_old) > 100000) { 
-                if(write_errors !=0) {
-                    SDLOGEXPDBG("E%d", write_errors); 
+                if(datafile_state.write_errors !=0) {
+                    SDLOGEXPDBG("E%d", datafile_state.write_errors); 
                 } else {
                     SDLOGEXPDBG("x");
                 }
@@ -168,10 +176,10 @@ msg_t sdlog_thread(void *p) {
 #endif
 
         } else {
-            if(sd_log_opened) {
-                ret = f_close(&DATAFil);       // might be redundant if card removed....\sa f_sync
+            if(datafile_state.sd_log_opened) {
+                ret = f_close(&datafile_state.DATAFil);       // might be redundant if card removed....\sa f_sync
                 SDLOGEXPDBG( "close file ret: %d\r\n", ret);
-                sd_log_opened = false;
+                datafile_state.sd_log_opened = false;
             }
         }
         chThdSleepMilliseconds(sdlog_thread_sleeptime_ms);
