f5dc4ee7609bfea7520d164c54ae023a80cb80ba
TheoH <Theo0x48@gmail.com>
2013-11-16 23:03:36 -0800

    Updated pressure_sensor to use new usbdetail

--- projects/pressure_sensor/cmddetail.h
+++ projects/pressure_sensor/cmddetail.h
@@ -18,15 +18,8 @@
 extern "C" {
 #endif
 
-
-#define SHELL_WA_SIZE   THD_WA_SIZE(2048)
-#define TEST_WA_SIZE    THD_WA_SIZE(256)
-
-
 void cmd_mem(BaseSequentialStream *chp, int argc, char *argv[]);
 void cmd_threads(BaseSequentialStream *chp, int argc, char *argv[]);
-
-
 
 #ifdef __cplusplus
 }
