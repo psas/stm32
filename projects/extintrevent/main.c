f2fd165b8ebcb9ba220b401a26c8acd9da45e9bd
TheoH <Theo0x48@gmail.com>
2013-11-26 19:19:54 -0800

    Refactored extintrevent to focus more around demoing events

--- projects/extintrevent/main.c
+++ projects/extintrevent/main.c
@@ -1,25 +1,3 @@
-/*
-    ChibiOS/RT - Copyright (C) 2006,2007,2008,2009,2010,
-                 2011,2012 Giovanni Di Sirio.
-
-    This file is part of ChibiOS/RT.
-
-    ChibiOS/RT is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
-    (at your option) any later version.
-
-    ChibiOS/RT is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-#include <stdio.h>
-#include <string.h>
 
 #include "ch.h"
 #include "hal.h"
@@ -27,141 +5,47 @@
 #include "usbdetail.h"
 #include "chprintf.h"
 #include "shell.h"
+#include "cmddetail.h"
+
+static EventSource button_event;
+static EventSource shell_event;
+//static EventSource timer_event; ?
 
 
-/* Olimex stm32-e407 board */
-
-static EventSource wkup_event;
-
-/*! \sa HAL_USE_EXT in hal_conf.h
- */
-static void green_led_off(void *arg) {
-
-  (void)arg;
-  palSetPad(GPIOC, GPIOC_LED);
+void cmd_event(BaseSequentialStream *chp, int argc, char *argv[]){
+    chprintf(chp, "Sending shell event \r\n");
+    /* Call the normal version because we're not in an ISR */
+    chEvtBroadcast(&shell_event);
 }
 
-/* Triggered when the WKUP button is pressed or released. The LED is set to ON.*/
-
-/* Challenge: Add de-bouncing */
-static void extcb1(EXTDriver *extp, expchannel_t channel) {
-  static VirtualTimer vt4;
+/* Triggered when the button is pressed or released. The LED is toggled.*/
+static void ext_cb(EXTDriver *extp, expchannel_t channel) {
 
   (void)extp;
   (void)channel;
 
-  palClearPad(GPIOC, GPIOC_LED);
+  palTogglePad(GPIOC, GPIOC_LED);
+
+  /* It is good practice to invoke this API before invoking any I-class
+   * syscall from an interrupt handler.
+   */
   chSysLockFromIsr();
-  chEvtBroadcastI(&wkup_event);
-
-  if (chVTIsArmedI(&vt4))
-    chVTResetI(&vt4);
-
-  /* LED4 set to OFF after 500mS.*/
-  chVTSetI(&vt4, MS2ST(500), green_led_off, NULL);
+  /* Call the I-Class version because we're in an ISR */
+  chEvtBroadcastI(&button_event);
   chSysUnlockFromIsr();
 }
 
-static const EXTConfig extcfg = {
-  {
-    {EXT_CH_MODE_BOTH_EDGES | EXT_CH_MODE_AUTOSTART | EXT_MODE_GPIOA, extcb1},   // WKUP Button PA0
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL}
-  }
-};
-
-/*===========================================================================*/
-/* Command line related.                                                     */
-/*===========================================================================*/
-
-static void cmd_mem(BaseSequentialStream *chp, int argc, char *argv[]) {
-  size_t n, size;
-
-  (void)argv;
-  if (argc > 0) {
-    chprintf(chp, "Usage: mem\r\n");
-    return;
-  }
-  n = chHeapStatus(NULL, &size);
-  chprintf(chp, "core free memory : %u bytes\r\n", chCoreStatus());
-  chprintf(chp, "heap fragments   : %u\r\n", n);
-  chprintf(chp, "heap free total  : %u bytes\r\n", size);
-}
-
-static void cmd_threads(BaseSequentialStream *chp, int argc, char *argv[]) {
-  static const char *states[] = {THD_STATE_NAMES};
-  Thread *tp;
-
-  (void)argv;
-  if (argc > 0) {
-    chprintf(chp, "Usage: threads\r\n");
-    return;
-  }
-  chprintf(chp, "    addr    stack prio refs     state time\r\n");
-  tp = chRegFirstThread();
-  do {
-    chprintf(chp, "%.8lx %.8lx %4lu %4lu %9s %lu\r\n",
-            (uint32_t)tp, (uint32_t)tp->p_ctx.r13,
-            (uint32_t)tp->p_prio, (uint32_t)(tp->p_refs - 1),
-            states[tp->p_state], (uint32_t)tp->p_time);
-    tp = chRegNextThread(tp);
-  } while (tp != NULL);
-}
-
-/*
- * Green LED blinker thread, times are in milliseconds.
- */
-static WORKING_AREA(waThread1, 128);
-static msg_t Thread1(void *arg) {
-
-  (void)arg;
-  chRegSetThreadName("blinker");
-  while (TRUE) {
-    palTogglePad(GPIOC, GPIOC_LED);
-    chThdSleepMilliseconds(500);
-  }
-  return -1;
-}
-
-/*
- * WKUP button handler
- *
- * Challenge: Do something more interesting here.
- */
-static void WKUP_button_handler(eventid_t id) {
-
+static void button_handler(eventid_t id) {
 	BaseSequentialStream *chp = getActiveUsbSerialStream();
-	chprintf(chp, "WKUP btn. eventid: %d\r\n", id);
+	chprintf(chp, "Button eventid: %d\r\n", id);
 }
 
-/*
- * Application entry point.
- */
-int main(void) {
-  static const evhandler_t evhndl[] = {
-    WKUP_button_handler
-  };
-  struct EventListener el0;
+static void shell_handler(eventid_t id){
+    BaseSequentialStream *chp = getActiveUsbSerialStream();
+    chprintf(chp, "Shell eventid: %d\r\n", id);
+}
+
+void main(void) {
   /*
    * System initializations.
    * - HAL initialization, this also initializes the configured device drivers
@@ -172,38 +56,63 @@ int main(void) {
   halInit();
   chSysInit();
 
-  /*
-   * Initialize event structures BEFORE using them
-   */
-  chEvtInit(&wkup_event);
+  /* Initialize event structures BEFORE using them */
+  chEvtInit(&button_event);
+  chEvtInit(&shell_event);
 
-  /*
-   * Challenge: add additional command line functions
+  /* As far as I can tell, each event source needs 0 or more unique event listeners
+   * - that is event listeners can't be shared.
    */
+
+  struct EventListener el0, el1;
+  chEvtRegister(&button_event, &el0, 0);
+  chEvtRegister(&shell_event, &el1, 1);
+
+  /* Start the USB serial shell */
   static const ShellCommand commands[] = {
     {"mem", cmd_mem},
     {"threads", cmd_threads},
+    {"event", cmd_event},
     {NULL, NULL}
   };
 
   usbSerialShellStart(commands);
-  /*
-   * Activates the EXT driver 1.
-   * This is for the external interrupt
-   */
+
+  /* Activate the external interrupt driver */
+  static EXTConfig extcfg = {
+    {
+      {EXT_CH_MODE_BOTH_EDGES | EXT_CH_MODE_AUTOSTART | EXT_MODE_GPIOA, ext_cb},   // WKUP Button PA0
+      {EXT_CH_MODE_DISABLED, NULL},
+      {EXT_CH_MODE_DISABLED, NULL},
+      {EXT_CH_MODE_DISABLED, NULL},
+      {EXT_CH_MODE_DISABLED, NULL},
+      {EXT_CH_MODE_DISABLED, NULL},
+      {EXT_CH_MODE_DISABLED, NULL},
+      {EXT_CH_MODE_DISABLED, NULL},
+      {EXT_CH_MODE_DISABLED, NULL},
+      {EXT_CH_MODE_DISABLED, NULL},
+      {EXT_CH_MODE_DISABLED, NULL},
+      {EXT_CH_MODE_DISABLED, NULL},
+      {EXT_CH_MODE_DISABLED, NULL},
+      {EXT_CH_MODE_DISABLED, NULL},
+      {EXT_CH_MODE_DISABLED, NULL},
+      {EXT_CH_MODE_DISABLED, NULL},
+      {EXT_CH_MODE_DISABLED, NULL},
+      {EXT_CH_MODE_DISABLED, NULL},
+      {EXT_CH_MODE_DISABLED, NULL},
+      {EXT_CH_MODE_DISABLED, NULL},
+      {EXT_CH_MODE_DISABLED, NULL},
+      {EXT_CH_MODE_DISABLED, NULL},
+      {EXT_CH_MODE_DISABLED, NULL}
+    }
+  };
   extStart(&EXTD1, &extcfg);
 
-  /*
-   * Creates the blinker thread.
-   */
-  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
-
-  /*
-   * Normal main() thread activity, in this demo it does nothing except
-   * sleeping in a loop and listen for events.
-   */
-  chEvtRegister(&wkup_event, &el0, 0);
-
+  /* Run the event loop */
+  const evhandler_t evhndl[] = {
+    button_handler,
+    shell_handler
+  };
   while (TRUE) {
     chEvtDispatch(evhndl, chEvtWaitOneTimeout(ALL_EVENTS, MS2ST(500)));
   }
