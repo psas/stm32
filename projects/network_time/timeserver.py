bfa9a66bec9e5c527006d14f3aa6c47633c6bb8a
David Ramage <dramage@comcast.net>
2013-11-30 16:09:19 -0800

    Should theoretically work now, beginning work on client-side code for Olimex board

--- projects/network_time/timeserver.py
+++ projects/network_time/timeserver.py
@@ -3,22 +3,35 @@ import socket
 import time
 import math
 from struct import *
-from enum import enum
+from enum import Enum
 
 class MsgType(Enum):
 	t1 = 1
 	t2 = 2
 
-IP_ADDR = "127.0.0.1"
+IP_ADDR = "10.0.0.2"
 SERVER_LISTEN_PORT = 31337
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 sock.bind((IP_ADDR, SERVER_LISTEN_PORT))
 
 while True:
-	data, addr = sock.recvfrom(1024) #
+	data, addr = sock.recvfrom(136) #
+	(msgType,t1Secs,t1MS,t2Secs,t2MS) = unpack("!cIIII",data)  #network byte order, char followed by 4 unsigned ints
+	msgType = int(msgType,8) #make sure its treated as an int
 	currenttime = time.time()
-	(msecs,secs) = math.modf(currenttime)
-	msecs = msecs * 1000
 	print "received message:", data
-	print "packet received at:", msecs, secs
+	if msgType == MsgType.t1:
+		respSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		currenttime = time.time()
+		(msecs,secs) = math.modf(currenttime)
+		msecs = msecs * 1000
+		response = pack("!cIIII",1,secs,msecs,0,0)
+		respSock.sendto(response(addr,31338))
+	elif msgType == MsgType.t2:
+		respSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		currenttime = time.time()
+		(msecs,secs) = math.modf(currenttime)
+		msecs = msecs * 1000
+		response = pack("!cIIII",1,secs,msecs,0,0)
+		respSock.sendto(response(addr,31338))
