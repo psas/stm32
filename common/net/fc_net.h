1d1d2d7785f90f269e6b16cc1d0788b21f150fae
TheoH <Theo0x48@gmail.com>
2014-01-08 00:10:11 -0800

    fc_net.h now uses addrs useable by normal sockets

--- common/net/fc_net.h
+++ common/net/fc_net.h
@@ -8,10 +8,7 @@
 #define _FC_NET_H
 
 
-#if LWIP_NETCONN
-	#include "lwip/ip_addr.h"
-	#define         IP_PSAS_FC(p)           IP4_ADDR(p, 192, 168, 0, 200)
-#endif
+#define         IP_PSAS_FC           "192.168.0.200"
 #define         FC_LISTEN_PORT_IMU_A     36000   // different ports for different sensors?
 #define         FC_LISTEN_PORT_ROLL_CTL  36002
 
