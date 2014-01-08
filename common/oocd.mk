26f37eeca45e6588904912bd913320db12fae0e1
TheoH <Theo0x48@gmail.com>
2014-01-08 00:08:27 -0800

    Starting to add gdb target into the makefiles. Not sure if works yet

--- common/oocd.mk
+++ common/oocd.mk
@@ -3,15 +3,23 @@ OPENOCD = $(PSAS_OPENOCD)
 OPENOCD_PATH = /opt/psas/openocd/bin
 OPENOCD_SEARCH = $(OPENOCD_PATH)/../share/openocd/scripts
 OPENOCD_HEXFILE = $(BUILDDIR)/$(PROJECT).hex
+OOCD_CFG = olimex_stm32_e407.cfg
 
 write: $(OPENOCD_HEXFILE) write_ocd
 
 write_base:
-	sudo env PATH="$(OPENOCD_PATH):$(PATH)" openocd -s $(OPENOCD) -s $(OPENOCD_SEARCH) -f $(OOCD_CFG) -c "script oocd_prep.script" -c "flash write_image erase $(OPENOCD_HEXFILE)" -c "reset" -c "shutdown"
+	 env PATH="$(OPENOCD_PATH):$(PATH)" openocd -s $(OPENOCD) -s $(OPENOCD_SEARCH) -f $(OOCD_CFG) -c "script oocd_prep.script" -c "flash write_image erase $(OPENOCD_HEXFILE)" -c "reset" -c "shutdown"
 
 write_ocd: OOCD_CFG = olimex_stm32_e407.cfg
 write_ocd: write_base
 
 write_stl: OOCD_CFG = stlinkv2_stm32_e407.cfg
 write_stl: write_base
+
+debug:
+	openocd -s $(OPENOCD) -s $(OPENOCD_SEARCH) -f $(OOCD_CFG) 
+	#sleep 3
+gdb:
+	$(TRGT)gdb -q $(BUILDDIR)/$(PROJECT).elf 
+
 
