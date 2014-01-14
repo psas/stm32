f07be8b2d37c9839bbc92acb42d3f552dbbe83fa
aperiodic <dlp@aperiodic.org>
2014-01-13 20:34:39 -0800

    Add fn doc comments to extdetail.c

--- projects/servo_control/extdetail.c
+++ projects/servo_control/extdetail.c
@@ -69,14 +69,23 @@ static uint16_t wakeup_button_presses = 0;
  * ========================== **************************************************
  */
 
+/*
+ * This function initializes the events needed to respond to wakeup button
+ * presses and launch detect pin level changes.
+ */
 void extdetail_init() {
     chEvtInit(&wakeup_event);
     chEvtInit(&launch_detect_event);
 }
 
 
-// why is this seperate from extdetail_init?
-// could it be called from there?
+/*
+ * This function initializes the launch_detected global to reflect the current
+ * state of the launch_detect pin.
+ *
+ * Open questions: why is this seperate from extdetail_init? could it be called
+ * from there?
+ */
 void launch_detect_init() {
     int pinval1, pinval2;
 
@@ -87,18 +96,25 @@ void launch_detect_init() {
     chThdSleepMilliseconds(10);
     pinval2 = palReadPad(GPIOD, 11);
 
+    // FIXME: inconsistent with launch_detect_handler
     if ((pinval1 == PAL_LOW) && (pinval2 == PAL_LOW)) {
         launch_detected = true;
     }
 }
 
-
+/*
+ * This function handles the launch_detect_event broadcast by the
+ * launch_detect_isr. It re-reads the pin value as a form of basic debouncing,
+ * and then passes the launch_detected global by reference to
+ * data_udp_tx_launch_det, which sends out a launch detect message via ethernet
+ * if the global is still true.
+ */
 void launch_detect_handler(eventid_t _) {
     (void)_;
 
     int pinval = palReadPad(GPIOD, 11);
 
-    // inconsistent with launch_detect_init
+    // FIXME: inconsistent with launch_detect_init
     if (pinval == PAL_LOW) {
     	launch_detected = false;
     } else {
@@ -115,7 +131,13 @@ void launch_detect_handler(eventid_t _) {
 }
 
 
-// why do even have this around?
+/*
+ * This function handles the wakeup_event broadcast by the wakeup_button_isr. It
+ * pointlessly increments the wakeup_button_presses global variable, which is
+ * not read by anything ever.
+ *
+ * TODO: delete this or figure out why we can't.
+ */
 void wakeup_button_handler(eventid_t _) {
     (void)_;
 
@@ -128,6 +150,11 @@ void wakeup_button_handler(eventid_t _) {
 }
 
 
+/*
+ * This ISR is triggered by any level change on the wakeup button's pin. It just
+ * broadcasts the wakeup_event, which is handled by the wakeup_button_handler
+ * function above.
+ */
 void wakeup_button_isr(EXTDriver *extp, expchannel_t channel) {
 	(void)extp;
 	(void)channel;
@@ -139,7 +166,9 @@ void wakeup_button_isr(EXTDriver *extp, expchannel_t channel) {
 
 
 /*
- * Triggered when the pin changes state (both edges)
+ * This ISR is triggered by any level change on the launch detect pin. It just
+ * broadcasts the launch_detect_event, which is handled by the
+ * launch_detect_handler above.
  */
 void launch_detect_isr(EXTDriver *extp, expchannel_t channel) {
 
@@ -148,6 +177,5 @@ void launch_detect_isr(EXTDriver *extp, expchannel_t channel) {
 
     chSysLockFromIsr();
     chEvtBroadcastI(&launch_detect_event);
-
     chSysUnlockFromIsr();
 }
