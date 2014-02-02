06d6a935e420882e3c2264795c75499fae162524
TheoH <Theo0x48@gmail.com>
2014-02-02 02:42:36 -0800

    Removed local boardfile and cleaned up main in rnet_led

--- projects/rnet_leds/boards/PSAS_ROCKETNET_HUB_1_0/board.mk
+++
@@ -1,6 +0,0 @@
-# List of all the board related files.
-BOARDSRC = ./boards/PSAS_ROCKETNET_HUB_1_0/board.c
-
-# Required include directories
-BOARDINC = ./boards/PSAS_ROCKETNET_HUB_1_0
-
--- projects/rnet_leds/main.c
+++ projects/rnet_leds/main.c
@@ -1,22 +1,3 @@
-/*
-    ChibiOS/RT - Copyright (C) 2006,2007,2008,2009,2010,
-                 2011,2012 Giovanni Di Sirio.
-
-    This file is part of ChibiOS/RT.
-
-    ChibiOS/RT is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
-    (at your option) any later version.
-
-    ChibiOS/RT is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 
 #include "ch.h"
 #include "hal.h"
@@ -52,7 +33,7 @@ static void led_init(void) {
 /*
  * Application entry point.
  */
-int main(void) {
+void main(void) {
 
   /*
    * System initializations.
@@ -61,15 +42,8 @@ int main(void) {
    * - Kernel initialization, the main() function becomes a thread and the
    *   RTOS is active.
    */
-  halInit();
-  chSysInit();
-
-  /*
-   * Normal main() thread activity, in this demo it enables and disables the
-   * button EXT channel using 5 seconds intervals.
-   */
+    halInit();
+    chSysInit();
     led_init();
-    chThdSleepMilliseconds(5000);
-    exit(1);
 }
 
