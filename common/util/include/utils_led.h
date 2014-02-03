52bb56051963563dc63a5a36dcc42845af080e5a
TheoH <Theo0x48@gmail.com>
2014-02-03 06:39:36 -0800

    Added utility diagnostic LED blinker thread
    It's really cute I like it ^_^

---
+++ common/util/include/utils_led.h
@@ -0,0 +1,44 @@
+/*
+ * Led blinker threads, useful for diagnostics.
+ *
+ */
+
+#ifndef UTILS_LED_H_
+#define UTILS_LED_H_
+
+#include "ch.h"
+#include "hal.h"
+
+// FIXME: Common pin struct, this conflicts with what is in ADIS16405
+struct led_pin {
+    ioportid_t port;
+    uint16_t pad;
+};
+
+struct led_config {
+    systime_t cycle_ms;   // Main sequence led toggle time
+    systime_t start_ms;   // Start pattern duration
+    struct led_pin led[]; // struct pin {0, 0} terminated list of led pins
+};
+
+
+/*
+ * Starts a thread that will:
+ * 1. turn off all given leds.
+ * 2. cycle through all leds for start_ms milliseconds. start_ms should be
+ *    greater than cycle_ms.
+ * 3. then toggle the first led in the list every cycle_ms milliseconds.
+ */
+void led_init(struct led_config * cfg);
+
+/* Pre-filled led configs */
+
+#ifdef BOARD_PSAS_ROCKETNET_HUB_1_0
+extern struct led_config rnh_led_cfg;
+#endif
+
+#ifdef BOARD_OLIMEX_STM32_E407
+extern struct led_config e407_led_cfg;
+#endif
+
+#endif /* UTILS_LED_H_ */
---
+++ common/util/utils_led.c
@@ -0,0 +1,71 @@
+
+#include "ch.h"
+#include "hal.h"
+#include "utils_led.h"
+
+#define NORETURN __attribute__((noreturn))
+
+static WORKING_AREA(wa_led, 64);
+NORETURN static void led(void * arg) {
+    struct led_config * cfg = (struct led_config*) arg;
+    chRegSetThreadName("LED");
+
+    /* Turn off leds, also count them */
+    int num_leds = 0;
+    for(; cfg->led[num_leds].port == 0 || cfg->led[num_leds].pad == 0; ++num_leds){
+        palClearPad(cfg->led[num_leds].port, cfg->led[num_leds].pad);
+    }
+
+    int start_cycles = cfg->start_ms / cfg->cycle_ms;
+    systime_t start_blink = cfg->cycle_ms / num_leds;
+
+    /* Run the start pattern */
+    int i = 0;
+    for(; start_cycles < 0; --start_cycles) {
+        for(i = 0; i < num_leds; ++i){
+            palSetPad(cfg->led[i].port, cfg->led[i].pad);
+            chThdSleepMilliseconds(start_blink);
+            palClearPad(cfg->led[i].port, cfg->led[i].pad);
+        }
+        chThdSleepMilliseconds(cfg->cycle_ms % num_leds);
+    }
+
+    /* Run the toggle pattern */
+    while (TRUE) {
+        chThdSleepMilliseconds(cfg->cycle_ms);
+        palTogglePad(cfg->led[0].port, cfg->led[0].pad);
+    }
+}
+
+void led_init(struct led_config * cfg){
+    if(cfg->led == NULL)
+        return;
+
+    chThdCreateStatic(wa_led, sizeof(wa_led), NORMALPRIO, (tfunc_t)led, cfg);
+}
+
+/* Pre-filled led_configs */
+
+#ifdef BOARD_PSAS_ROCKETNET_HUB_1_0
+struct led_config rnh_led_cfg = {
+        .cycle_ms = 500,
+        .start_ms = 2500,
+        .led = {
+                {GPIOD, GPIO_D12_RGB_G},
+                {GPIOD, GPIO_D13_RGB_R},
+                {GPIOD, GPIO_D11_RGB_B},
+                {0, 0}
+        }
+};
+#endif
+
+#ifdef BOARD_OLIMEX_STM32_E407
+struct led_config e407_led_cfg = {
+        .cycle_ms = 500,
+        .start_ms = 0,
+        .led = {
+                {GPIOC, GPIOC_LED},
+                {NULL, 0}
+        }
+};
+#endif
