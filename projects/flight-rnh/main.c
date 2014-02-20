2f10234b3ef7061f569ffb9838b8158010974106
TheoH <Theo0x48@gmail.com>
2014-02-20 01:52:56 -0800

    Incorporated utils-led and the new BQ driver into flight-rnh

--- projects/flight-rnh/main.c
+++ projects/flight-rnh/main.c
@@ -7,6 +7,7 @@
 #include "RNH.h"
 #include "net_addrs.h"
 #include "utils_sockets.h"
+#include "utils_led.h"
 #include "rnet_cmd_interp.h"
 
 #include "lwip/ip_addr.h"
@@ -18,48 +19,6 @@
 #define NORETURN __attribute__((noreturn))
 
 #include "chprintf.h"
-
-static WORKING_AREA(led_area, 64);
-NORETURN static void led(void *arg UNUSED) {
-    const int led_wait_time = 500;
-
-    chRegSetThreadName("LED");
-
-    palClearPad(GPIOD, GPIO_D12_RGB_G);
-    palClearPad(GPIOD, GPIO_D13_RGB_R);
-    palClearPad(GPIOD, GPIO_D11_RGB_B);
-
-    int i = 0;
-    for(i=0; i<5; ++i) {
-        palClearPad(GPIOD, GPIO_D12_RGB_G);
-        chThdSleepMilliseconds(150);
-
-        palSetPad(GPIOD, GPIO_D12_RGB_G);
-        palClearPad(GPIOD, GPIO_D13_RGB_R);
-        chThdSleepMilliseconds(150);
-
-        palSetPad(GPIOD, GPIO_D13_RGB_R);
-        palClearPad(GPIOD, GPIO_D11_RGB_B);
-        chThdSleepMilliseconds(150);
-
-        palSetPad(GPIOD, GPIO_D11_RGB_B);
-        chThdSleepMilliseconds(50);
-    }
-
-    while (TRUE) {
-        chThdSleepMilliseconds(led_wait_time);
-        palTogglePad(GPIOD, GPIO_D12_RGB_G);
-    }
-}
-
-
-/*
- * Parts that we need:
- * eth terminal
- * logic (turn on/off port, etc)
- * Logger
- *
- */
 
 #define ETHERNET_COMMANDS_STACK_SIZE      1024
 WORKING_AREA(wa_ethernet_commands, ETHERNET_COMMANDS_STACK_SIZE);
@@ -146,6 +105,7 @@ void sleep(void){
 static void ACOK_cb(EXTDriver *extp UNUSED, expchannel_t channel UNUSED) {
 
     if(BQ24725_ACOK()){
+        BQ24725_start();
         KS8999_enable(TRUE);
     }else{
         RNH_power(RNH_PORT_ALL, RNH_PORT_OFF);
@@ -156,22 +116,23 @@ static void ACOK_cb(EXTDriver *extp UNUSED, expchannel_t channel UNUSED) {
 void main(void) {
     halInit();
     chSysInit();
-
-    //start the LED blinker thread
-    chThdCreateStatic(led_area, sizeof(led_area), NORMALPRIO, (tfunc_t)led, NULL);
+    led_init(&rnh_led_cfg); //diagnostic LED blinker
 
     //Init hardware
+
+    struct BQ24725Config BQConf = {
+            .ACOK = {GPIOD, GPIO_D0_BQ24_ACOK},
+            .ACOK_cb = ACOK_cb,
+            .I2CD = &I2CD1
+    };
+    BQ24725_init(&BQConf);
     if(BQ24725_ACOK()){
         BQ24725_start();
-        KS8999_init();
-        eth_start();
     }
-    //Start ACOK power events
-    EXTConfig bqextcfg;
-    memset(&bqextcfg, 0, sizeof(bqextcfg));
-    bqextcfg.channels[0].mode = EXT_CH_MODE_BOTH_EDGES | EXT_CH_MODE_AUTOSTART | EXT_MODE_GPIOD;
-    bqextcfg.channels[0].cb = &ACOK_cb;
-    extStart(&EXTD1, &bqextcfg);
+
+
+    KS8999_init();
+    eth_start();
 
     //main should never return
     while (TRUE) {
