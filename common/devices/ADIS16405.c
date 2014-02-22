af3b0681b3bc40dcd1e5f73f67acbf6c43f21714
TheoH <Theo0x48@gmail.com>
2014-02-21 16:19:12 -0800

    ADIS driver now takes config arg, also some minor name cleanup

--- common/devices/ADIS16405.c
+++ common/devices/ADIS16405.c
@@ -14,21 +14,23 @@
 
 #include "ADIS16405.h"
 
+#define UNUSED __attribute__((unused))
+
 #define WRITE_ADDR(addr) (addr) |  0x80
 #define READ_ADDR(addr)  (addr) & ~0x80
 
-static SPIDriver *SPID = NULL;
-static const adis_pins * PINS = NULL;
-EventSource adis_data_ready;
-uint8_t adis_raw_in[sizeof(ADIS16405_burst_data)+ 2];
+#define BURST_EXCHANGE_LEN sizeof(ADIS16405_burst_data)+ 2 //+2 for initial addr
+uint8_t adis_raw_in[BURST_EXCHANGE_LEN];
+EventSource ADIS16405_data_ready;
 
-const char adisid[(sizeof("ADIS")-1)] = "ADIS";
+static const ADIS16405Config * CONF;
 
-const adis_pins adis_olimex_e407 = {
+const ADIS16405Config adis_olimex_e407 = {
         .spi_cs = {GPIOA, GPIOA_PIN4},
         .spi_sck = {GPIOA, GPIOA_PIN5},
         .spi_miso = {GPIOA, GPIOA_PIN6},
         .spi_mosi = {GPIOB, GPIOB_PIN5},
+        .SPID = &SPID1,
         .reset = {GPIOD, GPIOD_PIN8},
         .dio1 = {GPIOD, GPIOD_PIN9},
         .dio2 = {GPIOD, GPIOD_PIN10},
@@ -61,122 +63,94 @@ static void buffer_to_burst_data(uint8_t * raw, ADIS16405_burst_data * data){
     data->aux_adc    = (raw[22] << 8 | raw[23]) & 0x0fff;
 }
 
-static void adis_data_rdy_cb(EXTDriver *extp __attribute__((unused)),
-                             expchannel_t channel __attribute__((unused)))
+static void adis_data_ready(EXTDriver *extp UNUSED, expchannel_t channel UNUSED)
 {
-    static uint8_t address[sizeof(ADIS16405_burst_data)+2] = {0x3E, 0};
-//    spiAcquireBus(SPID);TODO
+    static uint8_t address[BURST_EXCHANGE_LEN] = {0x3E, 0};
+//    spiAcquireBus(CONF->SPID);TODO
     chSysLockFromIsr();
-    spiSelectI(SPID);
-    spiStartExchangeI(SPID, sizeof(ADIS16405_burst_data)+2, address, adis_raw_in);
+    spiSelectI(CONF->SPID);
+    spiStartExchangeI(CONF->SPID, BURST_EXCHANGE_LEN, address, adis_raw_in);
     chSysUnlockFromIsr();
 }
 
-static void spi_complete_cb(SPIDriver * SPID){
+static void spi_complete(SPIDriver * SPID){
     chSysLockFromIsr();
     spiUnselectI(SPID);
 //    todo: timestamp here to measure delay in event broadcasts
-    chEvtBroadcastI(&adis_data_ready);
+    chEvtBroadcastI(&ADIS16405_data_ready);
     chSysUnlockFromIsr();
-//    spiReleaseBus(SPID); TODO
+//    spiReleaseBus(CONF->SPID); TODO
 }
 
-void adis_init(const adis_pins * pins) {
-
-    PINS = pins;
+void adis_init(const ADIS16405Config * conf) {
     uint32_t PINMODE = PAL_STM32_OTYPE_PUSHPULL | PAL_STM32_OSPEED_HIGHEST | PAL_STM32_PUDR_FLOATING;
     /* SPI pins setup */
-    palSetPadMode(pins->spi_sck.port, pins->spi_sck.pad, PAL_MODE_ALTERNATE(5) | PINMODE);
-    palSetPadMode(pins->spi_miso.port, pins->spi_miso.pad, PAL_MODE_ALTERNATE(5) | PINMODE);
-    palSetPadMode(pins->spi_mosi.port, pins->spi_mosi.pad, PAL_MODE_ALTERNATE(5) | PINMODE);
-    palSetPadMode(pins->spi_cs.port, pins->spi_cs.pad, PAL_MODE_OUTPUT_PUSHPULL | PINMODE);
-    palSetPad(pins->spi_cs.port, pins->spi_cs.pad); //unselect
+    palSetPadMode(conf->spi_sck.port, conf->spi_sck.pad, PAL_MODE_ALTERNATE(5) | PINMODE);
+    palSetPadMode(conf->spi_miso.port, conf->spi_miso.pad, PAL_MODE_ALTERNATE(5) | PINMODE);
+    palSetPadMode(conf->spi_mosi.port, conf->spi_mosi.pad, PAL_MODE_ALTERNATE(5) | PINMODE);
+    palSetPadMode(conf->spi_cs.port, conf->spi_cs.pad, PAL_MODE_OUTPUT_PUSHPULL | PINMODE);
+    palSetPad(conf->spi_cs.port, conf->spi_cs.pad); //unselect
     /* GPIO pins setup */
-    palSetPadMode(pins->reset.port, pins->reset.pad, PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
-    palSetPadMode(pins->dio1.port, pins->dio1.pad, PAL_MODE_INPUT_PULLDOWN | PAL_STM32_OSPEED_HIGHEST);
-    palSetPadMode(pins->dio2.port, pins->dio2.pad, PAL_MODE_INPUT_PULLDOWN | PAL_STM32_OSPEED_HIGHEST);
-    palSetPadMode(pins->dio3.port, pins->dio3.pad, PAL_MODE_INPUT_PULLDOWN | PAL_STM32_OSPEED_HIGHEST);
-    palSetPadMode(pins->dio4.port, pins->dio4.pad, PAL_MODE_INPUT_PULLDOWN | PAL_STM32_OSPEED_HIGHEST);
+    palSetPadMode(conf->reset.port, conf->reset.pad, PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
+    palSetPadMode(conf->dio1.port, conf->dio1.pad, PAL_MODE_INPUT_PULLDOWN | PAL_STM32_OSPEED_HIGHEST);
+    palSetPadMode(conf->dio2.port, conf->dio2.pad, PAL_MODE_INPUT_PULLDOWN | PAL_STM32_OSPEED_HIGHEST);
+    palSetPadMode(conf->dio3.port, conf->dio3.pad, PAL_MODE_INPUT_PULLDOWN | PAL_STM32_OSPEED_HIGHEST);
+    palSetPadMode(conf->dio4.port, conf->dio4.pad, PAL_MODE_INPUT_PULLDOWN | PAL_STM32_OSPEED_HIGHEST);
 
-
-    /*! \brief ADIS SPI configuration
-     *
+    /* ADIS SPI configuration
      * 656250Hz, CPHA=1, CPOL=1, MSb first.
-     *
      * For burst mode ADIS SPI is limited to 1Mhz.
      */
-
     static SPIConfig spicfg = {
-            .end_cb = spi_complete_cb,
+            .end_cb = spi_complete,
             .cr1 = SPI_CR1_CPOL | SPI_CR1_CPHA | SPI_CR1_BR_2 | SPI_CR1_BR_1
     };
-    spicfg.ssport = pins->spi_cs.port;
-    spicfg.sspad = pins->spi_cs.pad;
-    SPID = &SPID1;
-    spiStart(SPID, &spicfg);
-
+    spicfg.ssport = conf->spi_cs.port;
+    spicfg.sspad = conf->spi_cs.pad;
+    spiStart(conf->SPID, &spicfg);
 
     /* Enable the external interrupt */
-    chEvtInit(&adis_data_ready);
+    chEvtInit(&ADIS16405_data_ready);
+    extAddCallback(&(conf->dio1), EXT_CH_MODE_RISING_EDGE | EXT_CH_MODE_AUTOSTART, adis_data_ready);
+    extUtilsStart();
 
-    //TODO: Is there a better way than a deep copy of doing this so that it
-    //      doesn't leave a bunch of unused extconfigs laying around?
-    //      EXTDn.config is marked const...
-    static EXTConfig extconfig;
-    if(EXTD1.config != NULL){
-        int i;
-        for(i = 0; i < EXT_MAX_CHANNELS; ++i){
-            extconfig.channels[i].cb = EXTD1.config->channels[i].cb;
-            extconfig.channels[i].mode = EXTD1.config->channels[i].mode;
-        }
-    }
-
-    //set dio1
-    uint32_t CHANNEL_MODE = EXT_MODE_GPIOD;
-//    TODO:
-//    if(pins->dio1.port == GPIOA){
-//        CHANNEL_MODE = EXT_MODE_GPIOA;
-//    } else if(pins->dio1.port == GPIOB){
-
-    //TODO: a good way to not blindly overwrite the already existing channel cb/mode
-    extconfig.channels[pins->dio1.pad].cb = adis_data_rdy_cb;
-    extconfig.channels[pins->dio1.pad].mode = EXT_CH_MODE_RISING_EDGE | CHANNEL_MODE | EXT_CH_MODE_AUTOSTART;
-
-    extStart(&EXTD1, &extconfig);
+    CONF = conf;
 }
 
 uint16_t adis_get(adis_regaddr addr){ //todo: array
     uint8_t txbuf[2] = {addr, 0};
     uint8_t rxbuf[2];
 
-    spiAcquireBus(SPID);
-    spiSelect(SPID);
+    spiAcquireBus(CONF->SPID);
+    spiSelect(CONF->SPID);
 
-    spiSend(SPID, sizeof(txbuf), txbuf);
-    spiReceive(SPID, sizeof(rxbuf), rxbuf);
+    spiSend(CONF->SPID, sizeof(txbuf), txbuf);
+    spiReceive(CONF->SPID, sizeof(rxbuf), rxbuf);
 
-    spiUnselect(SPID);
-    spiReleaseBus(SPID);
+    spiUnselect(CONF->SPID);
+    spiReleaseBus(CONF->SPID);
 
     return rxbuf[0] << 8 | rxbuf[1];
 }
 
 void adis_set(adis_regaddr addr, uint16_t value){ //todo:array
-    uint8_t txbuf[4] = {WRITE_ADDR(addr+1), value >> 8, WRITE_ADDR(addr), value};
+    uint8_t txbuf[4] = {
+            WRITE_ADDR(addr+1), value >> 8,
+            WRITE_ADDR(addr),   value
+    };
+    spiAcquireBus(CONF->SPID);
+    spiSelect(CONF->SPID);
 
-    spiAcquireBus(SPID);
-    spiSelect(SPID);
+    spiSend(CONF->SPID, sizeof(txbuf), txbuf);
 
-    spiSend(SPID, sizeof(txbuf), txbuf);
-
-    spiUnselect(SPID);
-    spiReleaseBus(SPID);
+    spiUnselect(CONF->SPID);
+    spiReleaseBus(CONF->SPID);
 }
 
 void adis_get_data(ADIS16405_burst_data * data){ // TODO: adis error struct
     // provides last sample or 0s if no sample received.
     chSysLock();
-    buffer_to_burst_data(adis_raw_in + 2, data);
+    buffer_to_burst_data(adis_raw_in + 2, data); //first 2 bytes are padding
     chSysUnlock();
 }
 
@@ -190,7 +164,7 @@ void adis_get_data(ADIS16405_burst_data * data){ // TODO: adis error struct
  */
 void adis_reset() {
     const unsigned int ADIS_RESET_MSECS = 500;
-	palClearPad(PINS->reset.port, PINS->reset.pad);
+	palClearPad(CONF->reset.port, CONF->reset.pad);
 	chThdSleepMilliseconds(ADIS_RESET_MSECS);
-	palSetPad(PINS->reset.port, PINS->reset.pad);
+	palSetPad(CONF->reset.port, CONF->reset.pad);
 }
