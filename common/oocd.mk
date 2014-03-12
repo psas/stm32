920507ba34929cbe23221650eacae264636e71a1
TheoH <Theo0x48@gmail.com>
2014-03-11 18:35:46 -0700

    Updated OpenOCD build infrastructure to take advantage of v0.7.0 features

--- common/oocd.mk
+++ common/oocd.mk
@@ -1,16 +1,13 @@
-
-OPENOCD = $(PSAS_OPENOCD)
-OPENOCD_PATH = /opt/psas/openocd/bin
-OPENOCD_SEARCH = $(OPENOCD_PATH)/../share/openocd/scripts
+OPENOCD_DIR = $(PSAS_OPENOCD)
 OPENOCD_HEXFILE = $(BUILDDIR)/$(PROJECT).hex
-OOCD_CFG = olimex_stm32_e407.cfg
 GDB_ELF = $(BUILDDIR)/$(PROJECT).elf
+OOCD_CFG = stlinkv2_stm32_e407.cfg
 GDB_CFG = $(PSAS_COMMON)/gdboocd_ocd.cmd
 
 write: $(OPENOCD_HEXFILE) write_ocd
 
 write_base:
-	 env PATH="$(OPENOCD_PATH):$(PATH)" openocd -s $(OPENOCD) -s $(OPENOCD_SEARCH) -f $(OOCD_CFG) -c "script oocd_prep.script" -c "flash write_image erase $(OPENOCD_HEXFILE)" -c "reset" -c "shutdown"
+	openocd -s $(OPENOCD_DIR) -f $(OOCD_CFG) -c "program $(OPENOCD_HEXFILE) verify reset"
 
 write_ocd: OOCD_CFG = olimex_stm32_e407.cfg
 write_ocd: write_base
