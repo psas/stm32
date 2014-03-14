9882d3396b9ff1a90c7fa146449421642e994558
TheoH <Theo0x48@gmail.com>
2014-03-14 16:49:19 -0700

    Filled out missing utils_shell header
    Becasue apparently I forgot to put it in at all???

---
+++ common/util/include/utils_shell.h
@@ -0,0 +1,12 @@
+/*
+ * Utility shell functions
+ *
+ */
+
+#ifndef _UTILS_SHELL_H_
+#define _UTILS_SHELL_H_
+
+void cmd_mem(BaseSequentialStream *chp, int argc, char *argv[]);
+void cmd_threads(BaseSequentialStream *chp, int argc, char *argv[]);
+
+#endif
