acd46c69216a756467cb3f7b977c4249ea0d0c79
TheoH <Theo0x48@gmail.com>
2014-03-16 00:58:46 -0700

    utils_led now uses utils_hal's struct pin

--- common/util/include/utils_hal.h
+++ common/util/include/utils_hal.h
@@ -1,6 +1,8 @@
 #ifndef UTILS_HAL_H_
 #define UTILS_HAL_H_
 
+#include "hal.h"
+
 struct pin {
     ioportid_t port;
     uint16_t pad;
@@ -8,5 +10,8 @@ struct pin {
 
 void extUtilsStart(void);
 void extAddCallback(const struct pin * pin, uint32_t mode, extcallback_t cb);
+//FIXME: anything that wants utils_hal for the pin struct needs
+//       HAL_USE_EXT defined as TRUE for extcallback_t. This should either be
+//       turned on by including utils_hal or not depend on it at all.
 
 #endif /* UTILS_HAL_H_ */
