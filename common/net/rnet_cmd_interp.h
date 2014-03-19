06a82d17b2ca29544a97bbcee3ef735704110029
TheoH <Theo0x48@gmail.com>
2014-03-19 02:10:02 -0700

    Suggestions as a result of code review

--- common/net/rnet_cmd_interp.h
+++ common/net/rnet_cmd_interp.h
@@ -16,14 +16,11 @@
  */
 struct RCICmdData{
     const char * cmd_name;  // Name of the command
-    char * cmd_data;        // Data segment of a received command
+    const char * cmd_data;        // Data segment of a received command
     int cmd_len;            // Length of valid cmd_data
 
     char * return_data;     // Output. Place data here to send to addr in from
     int return_len;         // length of data in return_data. Max length TODO
-
-    struct sockaddr * from; // Address the command was sent from
-    socklen_t fromlen;      // Size of the address
 };
 
 /* RCI command handler function type*/
