f39fc5cc0c31526162da3b2395d71480d496e704
TheoH <Theo0x48@gmail.com>
2014-04-02 04:40:01 -0700

    Began I2C section in utils_hal
    It's still a work in progress, the bodies of functions are
    currently commented out,  but I wanted to throw this into git now.

--- common/util/include/utils_hal.h
+++ common/util/include/utils_hal.h
@@ -13,4 +13,16 @@ void extUtilsStart(void);
 void extAddCallback(const struct pin * pin, uint32_t mode, extcallback_t cb);
 #endif
 
+#if HAL_USE_I2C
+typedef struct{
+    struct pin SDA;
+    struct pin SCL;
+} I2CPins;
+
+void utils_i2cStart(I2CDriver * driver, I2CConfig * config, I2CPins * pins);
+void chprint_i2c_state(BaseSequentialStream * chp, i2cstate_t state);
+void chprint_i2c_error(BaseSequentialStream * chp, int err);
+#endif
+
+
 #endif /* UTILS_HAL_H_ */
