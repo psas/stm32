3c9f3619f2fb4bfacc191c4dcb92174cc067775b
TheoH <Theo0x48@gmail.com>
2014-04-02 01:33:18 -0700

    Added  to make including common/net sources in a makefile easy

--- common/psas.mk
+++ common/psas.mk
@@ -4,4 +4,5 @@ PSAS_OPENOCD       = ../../toolchain/openocd
 PSAS_DEVICES       = $(PSAS_COMMON)/devices
 PSAS_UTIL          = $(PSAS_COMMON)/util
 PSAS_NET           = $(PSAS_COMMON)/net
+PSAS_NETSRC        = $(PSAS_NET)/rnet_cmd_interp.c $(PSAS_NET)/net_addrs.c $(PSAS_NET)/utils_sockets.c
 PSAS_BOARDS        = $(PSAS_COMMON)/boards
