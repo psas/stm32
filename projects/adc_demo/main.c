bf68a2b2c2b5173db184157a16eedde39fab54ce
kwilson <kwilsonpdx@gmail.com>
2014-04-08 23:12:24 -0700

    adc_demo project started

---
+++ projects/adc_demo/main.c
@@ -0,0 +1,44 @@
+
+
+
+ADC_GRP1_NUM_CHANNELS 1
+
+
+/*
+ * ADC conversion group 1.
+ * Mode:       
+ * Channels:    IN10.
+ */
+static const ADCConversionGroup adcgrpcfg1 = {
+    .circular = FALSE,                                           // circular
+    ADC_GRP1_NUM_CHANNELS,                           
+    NULL,                                            // end cb
+    adcerrorcallback,                                // error cb
+    (ADC_CR1_DISCEN | ADC_CR1_EOCIE),                // CR1 
+    ADC_CR2_SWSTART,                                 // CR2  ??
+    ADC_SMPR1_SMP_AN11(ADC_SAMPLE_480),
+    0,                                               // SMPR2
+    ADC_SQR1_NUM_CH(ADC_GRP1_NUM_CHANNELS),
+    0,                                               // SQR2
+    ADC_SQR3_SQ1_N(ADC_CHANNEL_IN10)
+};
+
+/*
+ * ADC conversion group.
+ * Mode:        Linear buffer, 8 samples of 1 channel, SW triggered.
+ * Channels:    IN11.
+ */
+static const ADCConversionGroup adcgrpcfg2 = {
+    FALSE,
+    ADC_GRP1_NUM_CHANNELS,
+    NULL,
+    adcerrorcallback,
+    0,                        /* CR1 */
+    ADC_CR2_SWSTART,          /* CR2 */
+    ADC_SMPR1_SMP_AN11(ADC_SAMPLE_480),
+    0,                        /* SMPR2 */
+    ADC_SQR1_NUM_CH(ADC_GRP1_NUM_CHANNELS),
+    0,                        /* SQR2 */
+    ADC_SQR3_SQ1_N(ADC_CHANNEL_IN11)
+};
+
