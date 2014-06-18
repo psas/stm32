6cc0a701dc6ba0098a940701b832dc1fd3815b06
TheoH <Theo0x48@gmail.com>
2014-06-17 21:31:54 -0700

    Continued cleaning of e407_serial

--- projects/e407_serial/main.c
+++ projects/e407_serial/main.c
@@ -1,30 +1,12 @@
-/*
-   ChibiOS/RT - Copyright (C) 2006,2007,2008,2009,2010,
-   2011,2012 Giovanni Di Sirio.
-
-   This file is part of ChibiOS/RT.
-
-   ChibiOS/RT is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 3 of the License, or
-   (at your option) any later version.
-
-   ChibiOS/RT is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-   */
 #include <stdbool.h>
 #include <stdlib.h>
 #include "ch.h"
 #include "hal.h"
 #include "chprintf.h"
 #include "shell.h"
-#include "cmddetail.h"
 
+#include "utils_general.h"
+#include "utils_led.h"
 #include "utils_shell.h"
 
 /*! The goal of this code is to run the shell through the serial terminal
@@ -33,7 +15,7 @@
  *
  * On the e407 the serial6 pins on portc6,7 are found on the UEXT connector.
  * See the schematic for pinout.
- *     stm		uext      ftdi
+ *     stm      uext      ftdi
  * TX: pc6      pin3      rx (yellow)
  * RX: pc7      pin4      tx (orange)
  *
@@ -43,66 +25,38 @@
  * In the halconf.h enable the serial system
  * #define HAL_USE_SERIAL                      TRUE
  */
-
-
-static uint32_t           led_wait_time         =        500;
+#define SHELL_WA_SIZE   THD_WA_SIZE(2048)
+void cmd_show(BaseSequentialStream *chp, int argc UNUSED, char *argv[] UNUSED) {
+	chprintf(chp, "I am the E407 Board.\r\n");
+}
 
 static const ShellCommand commands[] = {
-		{"show", cmd_show},
-		{"phy_read", cmd_phy_read},
-		{"find_phy", cmd_find_phy},
-		{"mem", cmd_mem},
-		{"threads", cmd_threads},
-		{NULL, NULL}
+	{"show", cmd_show},
+	{"mem", cmd_mem},
+	{"threads", cmd_threads},
+	{NULL, NULL}
 };
 
 static const ShellConfig shell_cfg1 = {
-		(BaseSequentialStream *)&SD6,
-		commands
+	(BaseSequentialStream *)&SD6,
+	commands
 };
 
-static WORKING_AREA(waThread_blinker, 64);
-/*! \brief Green LED blinker thread
-*/
-static msg_t Thread_blinker(void *arg) {
-    (void)arg;
-    chRegSetThreadName("blinker");
-    while (TRUE) {
-        palTogglePad(GPIOC, GPIOC_LED);
-        chThdSleepMilliseconds(led_wait_time);
-    }
-    return -1;
-}
+void main(void) {
 
-
-int main(void) {
-    static Thread            *shelltp       = NULL;
-
-    /*
-     * System initializations.
-     * - HAL initialization, this also initializes the configured device drivers
-     *   and performs the board-specific initializations.
-     * - Kernel initialization, the main() function becomes a thread and the
-     *   RTOS is active.
-     */
     halInit();
     chSysInit();
-
+    ledStart(NULL);
     // start the serial port
     sdStart(&SD6, NULL);
-    chThdSleepMilliseconds(300);
-    /*
-     * Shell manager initialization.
-     */
+    // Shell manager initialization.
     shellInit();
 
-    chThdCreateStatic(waThread_blinker          , sizeof(waThread_blinker)          , NORMALPRIO    , Thread_blinker         , NULL);
-
     /*
-     * Normal main() thread activity, in this demo it enables and disables the
-     * button EXT channel using 5 seconds intervals.
+     * Normal main() thread activity, does something with the shell it looks like
      */
 
+    static Thread *shelltp = NULL;
     while (true) {
         if (!shelltp )
             shelltp = shellCreate(&shell_cfg1, SHELL_WA_SIZE, NORMALPRIO);
@@ -112,7 +66,5 @@ int main(void) {
         }
         chThdSleepMilliseconds(1000);
     }
-
-    exit(0);
 }
 
