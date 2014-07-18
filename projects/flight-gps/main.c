c3bcae5eee2b043c2d59c5eade97727f8a7da60b
TheoH <Theo0x48@gmail.com>
2014-07-18 04:50:22 -0700

    Removed attepts at framing from flight-gps

--- projects/flight-gps/main.c
+++ projects/flight-gps/main.c
@@ -8,25 +8,9 @@
 #include "utils_sockets.h"
 #include "utils_led.h"
 
-static struct SeqSocket cots_socket = DECL_SEQ_SOCKET(400);
 
-static void cots_handler(eventid_t id UNUSED){
-	static int len = 0;
-	int i = 0;
-	do {
-		len += sdAsynchronousRead(&SD6, cots_socket.buffer + len, 1);
-		if(len <= 0){
-			return;
-		}
-		for(; i+1 < len; ++i){
-			if((cots_socket.buffer[i] == '\r' && cots_socket.buffer[i + 1] == '\n') || len >= 400){
-				seq_write(&cots_socket, len);
-				len = 0;
-				return;
-			}
-		}
-	} while(TRUE);
-}
+#define COTS_BUFFER_SIZE 1400
+static struct SeqSocket cots_socket = DECL_SEQ_SOCKET(COTS_BUFFER_SIZE);
 
 void main(void) {
 
@@ -47,31 +31,10 @@ void main(void) {
 	seq_socket_init(&cots_socket, s);
 	connect(cots_socket.socket, FC_ADDR, sizeof(struct sockaddr));
 
-
-
-	struct EventListener elCots;
-	static const evhandler_t evhndl[] = {
-		cots_handler
-	};
-	chEvtRegister(chnGetEventSource(&SD6), &elCots, 0);
-
-
 	while(TRUE){
-		eventmask_t events = chEvtWaitAny(ALL_EVENTS);
-		if(events & 1 << 0){
-			ledToggle(&RED);
-		} else if(events & 1 << 1) {
-			ledToggle(&BLUE);
-		} else if(events & 1 << 2) {
-			ledToggle(&GREEN);
-		} else if(events & 1 << 3) {
-			ledToggle(&LED2);
-		} else if(events & 1 << 4) {
-			ledToggle(&LED4);
-		} else if(events & 1 << 5) {
-			ledToggle(&LED5);
-		}
-		chEvtDispatch(evhndl, events);
+		int len = sdReadTimeout(&SD6, cots_socket.buffer, COTS_BUFFER_SIZE, S2ST(1));
+		seq_write(&cots_socket, len);
+		ledToggle(&RED);
 	}
 }
 
