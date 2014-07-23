72350756faa446b22f2a7672d5f4db970b06f4d2
TheoH <Theo0x48@gmail.com>
2014-07-23 13:32:09 -0700

    Added intro usbserial shell project

---
+++ projects/usbserial/main.c
@@ -0,0 +1,26 @@
+#include "ch.h"
+#include "hal.h"
+
+#include "usbdetail.h"
+#include "utils_led.h"
+
+/* Application entry point */
+void main(void) {
+    /* System initializations.
+     * - HAL initialization, this also initializes the configured device drivers
+     *   and performs the board-specific initializations. Config files for these
+     *   are located in halconf.h and mcuconf.h
+     */
+    halInit();
+    /* - Kernel initialization, the main() function becomes a thread and the
+     *   RTOS is active. The kernel config file is located in chconf.h
+     */
+    chSysInit();
+    ledStart(NULL);
+    usbSerialShellStart(NULL);
+    /* main() should never return */
+    while(TRUE){
+        chThdSleep(TIME_INFINITE);
+    }
+}
+
