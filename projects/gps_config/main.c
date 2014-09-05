958823bd95c3a794487260625a5a9c6158ecd795
TheoH <Theo0x48@gmail.com>
2014-09-05 16:17:59 -0700

    MAX apparently needs registers set to default first to work
    I think it might just be one of the stream bits, but I'll refine
    later. Also improved formatting

--- projects/gps_config/main.c
+++ projects/gps_config/main.c
@@ -12,89 +12,97 @@
 #include "MAX2769.h"
 
 static void max2769_config(void) {
-	int conf1       =  0;
-	int conf2       =  0;
-	int conf3       =  0;
-	int pllconf     =  0;
-	int pllidr      =  0;
-	int cfdr        =  0;
-
 	max2769_reset();
+
 	// Configuration from Google doc MAX2769RegisterConfiguration
-	conf1 = (0b1            << MAX2769_CONF1_CHIPEN ) |
-	        (0b0            << MAX2769_CONF1_IDLE   ) |
-	        (0b1111         << MAX2769_CONF1_ILNA1  ) |
-	        (0b00           << MAX2769_CONF1_ILNA2  ) |
-	        (0b11           << MAX2769_CONF1_ILO    ) |
-	        (0b11           << MAX2769_CONF1_IMIX   ) |
-	        (0b0            << MAX2769_CONF1_MIXPOLE) |
-	        (0b10           << MAX2769_CONF1_LNAMODE) |
-	        (0b1            << MAX2769_CONF1_MIXEN  ) |
-	        (0b0            << MAX2769_CONF1_ANTEN  ) |
-	        (0b001101       << MAX2769_CONF1_FCEN   ) |
-	        (0b00           << MAX2769_CONF1_FBW    ) |
-	        (0b0            << MAX2769_CONF1_F3OR5  ) |
-	        (0b1            << MAX2769_CONF1_FCENX  ) |
-	        (0b1            << MAX2769_CONF1_FGAIN  );
+	uint32_t conf1 =
+		(0b1            << MAX2769_CONF1_CHIPEN ) |
+		(0b0            << MAX2769_CONF1_IDLE   ) |
+		(0b1111         << MAX2769_CONF1_ILNA1  ) |
+		(0b00           << MAX2769_CONF1_ILNA2  ) |
+		(0b11           << MAX2769_CONF1_ILO    ) |
+		(0b11           << MAX2769_CONF1_IMIX   ) |
+		(0b0            << MAX2769_CONF1_MIXPOLE) |
+		(0b10           << MAX2769_CONF1_LNAMODE) |
+		(0b1            << MAX2769_CONF1_MIXEN  ) |
+		(0b0            << MAX2769_CONF1_ANTEN  ) |
+		(0b001101       << MAX2769_CONF1_FCEN   ) |
+		(0b00           << MAX2769_CONF1_FBW    ) |
+		(0b0            << MAX2769_CONF1_F3OR5  ) |
+		(0b1            << MAX2769_CONF1_FCENX  ) |
+		(0b1            << MAX2769_CONF1_FGAIN  );
 
-	conf2 = (0b1            << MAX2769_CONF2_IQEN    ) |
-	        (0b10101010     << MAX2769_CONF2_GAINREF ) |
-	        (0b00           << MAX2769_CONF2_AGCMODE ) |
-	        (0b01           << MAX2769_CONF2_FORMAT  ) |
-	        (0b010          << MAX2769_CONF2_BITS    ) |
-	        (0b00           << MAX2769_CONF2_DRVCFG  ) |
-	        (0b1            << MAX2769_CONF2_LOEN    ) |
-	        (0b00           << MAX2769_CONF2_DIEID   );
+	uint32_t conf2 =
+		(0b1            << MAX2769_CONF2_IQEN   ) |
+		(0b10101010     << MAX2769_CONF2_GAINREF) |
+		(0b00           << MAX2769_CONF2_AGCMODE) |
+		(0b01           << MAX2769_CONF2_FORMAT ) |
+		(0b010          << MAX2769_CONF2_BITS   ) |
+		(0b00           << MAX2769_CONF2_DRVCFG ) |
+		(0b1            << MAX2769_CONF2_LOEN   ) |
+		(0b00           << MAX2769_CONF2_DIEID  );
 
-	conf3 = (0b111010       << MAX2769_CONF3_GAININ      ) |
-	        (0b1            << MAX2769_CONF3_FSLOWEN     ) |
-	        (0b0            << MAX2769_CONF3_HILOADEN    ) |
-	        (0b1            << MAX2769_CONF3_ADCEN       ) |
-	        (0b1            << MAX2769_CONF3_DRVEN       ) |
-	        (0b1            << MAX2769_CONF3_FOFSTEN     ) |
-	        (0b1            << MAX2769_CONF3_FILTEN      ) |
-	        (0b1            << MAX2769_CONF3_FHIPEN      ) |
-	        (0b1            << MAX2769_CONF3_PGAIEN      ) |
-	        (0b1            << MAX2769_CONF3_PGAQEN      ) |
-	        (0b1            << MAX2769_CONF3_STRMEN      ) |
-	        (0b0            << MAX2769_CONF3_STRMSTART   ) |
-	        (0b0            << MAX2769_CONF3_STRMSTOP    ) |
-	        (0b000          << MAX2769_CONF3_STRMCOUNT   ) |
-	        (0b11           << MAX2769_CONF3_STRMBITS    ) |
-	        (0b0            << MAX2769_CONF3_STAMPEN     ) |
-	        (0b1            << MAX2769_CONF3_TIMESYNCEN  ) |
-	        (0b1            << MAX2769_CONF3_DATASYNCEN  ) |
-	        (0b0            << MAX2769_CONF3_STRMRST     );
+	uint32_t conf3 =
+		(0b111010       << MAX2769_CONF3_GAININ    ) |
+		(0b1            << MAX2769_CONF3_FSLOWEN   ) |
+		(0b0            << MAX2769_CONF3_HILOADEN  ) |
+		(0b1            << MAX2769_CONF3_ADCEN     ) |
+		(0b1            << MAX2769_CONF3_DRVEN     ) |
+		(0b1            << MAX2769_CONF3_FOFSTEN   ) |
+		(0b1            << MAX2769_CONF3_FILTEN    ) |
+		(0b1            << MAX2769_CONF3_FHIPEN    ) |
+		(0b1            << MAX2769_CONF3_PGAIEN    ) |
+		(0b1            << MAX2769_CONF3_PGAQEN    ) |
+		(0b1            << MAX2769_CONF3_STRMEN    ) |
+		(0b0            << MAX2769_CONF3_STRMSTART ) |
+		(0b0            << MAX2769_CONF3_STRMSTOP  ) |
+		(0b000          << MAX2769_CONF3_STRMCOUNT ) |
+		(0b11           << MAX2769_CONF3_STRMBITS  ) |
+		(0b0            << MAX2769_CONF3_STAMPEN   ) |
+		(0b1            << MAX2769_CONF3_TIMESYNCEN) |
+		(0b1            << MAX2769_CONF3_DATASYNCEN) |
+		(0b0            << MAX2769_CONF3_STRMRST   );
 
-	pllconf = (0b1          << MAX2769_PLL_VCOEN)    |
-	          (0b0          << MAX2769_PLL_IVCO)     |
-	          (0b1          << MAX2769_PLL_REFOUTEN) |
-	          (0b01         << MAX2769_PLL_REFDIV)   |
-	          (0b01         << MAX2769_PLL_IXTAL)    |
-	          (0b1000       << MAX2769_PLL_XTALCAP)  |
-	          (0b0000       << MAX2769_PLL_LDMUX)    |
-	          (0b0          << MAX2769_PLL_ICP)      |
-	          (0b0          << MAX2769_PLL_PFDEN)    |
-	          (0b000        << MAX2769_PLL_CPTEST)   |
-	          (0b1          << MAX2769_PLL_INT_PLL)  |
-	          (0b0          << MAX2769_PLL_PWRSAV);
+	uint32_t pllconf =
+		(0b1          << MAX2769_PLL_VCOEN   ) |
+		(0b0          << MAX2769_PLL_IVCO    ) |
+		(0b1          << MAX2769_PLL_REFOUTEN) |
+		(0b01         << MAX2769_PLL_REFDIV  ) |
+		(0b01         << MAX2769_PLL_IXTAL   ) |
+		(0b1000       << MAX2769_PLL_XTALCAP ) |
+		(0b0000       << MAX2769_PLL_LDMUX   ) |
+		(0b0          << MAX2769_PLL_ICP     ) |
+		(0b0          << MAX2769_PLL_PFDEN   ) |
+		(0b000        << MAX2769_PLL_CPTEST  ) |
+		(0b1          << MAX2769_PLL_INT_PLL ) |
+		(0b0          << MAX2769_PLL_PWRSAV  );
 
-	pllidr = (1536          << MAX2769_PLLIDR_NDIV) |
-	         (16            << MAX2769_PLLIDR_RDIV);
+	uint32_t pllidr =
+		(1536         << MAX2769_PLLIDR_NDIV) |
+		(16           << MAX2769_PLLIDR_RDIV);
 
-	cfdr = (256             << MAX2769_CFDR_L_CNT)   |
-	       (1563            << MAX2769_CFDR_M_CNT)   |
-	       (0               << MAX2769_CFDR_FCLKIN)  |
-	       (1               << MAX2769_CFDR_ADCCLK)  |
-	       (0               << MAX2769_CFDR_SERCLK)  |
-	       (1               << MAX2769_CFDR_MODE);
+	uint32_t cfdr =
+		(256          << MAX2769_CFDR_L_CNT ) |
+		(1563         << MAX2769_CFDR_M_CNT ) |
+		(0            << MAX2769_CFDR_FCLKIN) |
+		(1            << MAX2769_CFDR_ADCCLK) |
+		(0            << MAX2769_CFDR_SERCLK) |
+		(1            << MAX2769_CFDR_MODE  );
 
-	max2769_set(MAX2769_CONF1, conf1 );
-	max2769_set(MAX2769_CONF2, conf2 );
-	max2769_set(MAX2769_CONF3, conf3 );
-	max2769_set(MAX2769_PLLCONF, pllconf );
-	max2769_set(MAX2769_PLLIDR, pllidr );
-	max2769_set(MAX2769_CFDR, cfdr );
+
+	max2769_set(MAX2769_CONF1, MAX2769_CONF1_DEF);
+	max2769_set(MAX2769_CONF2, MAX2769_CONF2_DEF);
+	max2769_set(MAX2769_CONF3, MAX2769_CONF3_DEF);
+	max2769_set(MAX2769_PLLCONF, MAX2769_PLLCONF_DEF);
+	max2769_set(MAX2769_PLLIDR, MAX2769_PLLIDR_DEF);
+	max2769_set(MAX2769_CFDR, MAX2769_CFDR_DEF);
+
+	max2769_set(MAX2769_CONF1, conf1);
+	max2769_set(MAX2769_CONF2, conf2);
+	max2769_set(MAX2769_CONF3, conf3);
+	max2769_set(MAX2769_PLLCONF, pllconf);
+	max2769_set(MAX2769_PLLIDR, pllidr);
+	max2769_set(MAX2769_CFDR, cfdr);
 	max2769_set(MAX2769_CONF3, conf3 | (0b1 << MAX2769_CONF3_STRMSTART));
 }
 
