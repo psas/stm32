ba5e9899d7943c0faa77fd71e1fed2b065298dea
TheoH <Theo0x48@gmail.com>
2015-07-12 20:10:22 -0700

    Venus hostside config script

---
+++ projects/flight-gps/venus-config.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+import serial, time
+from struct import pack
+from contextlib import contextmanager
+import socket
+"""
+Connects to a SkyTraq Venus on ttyUSB0 and sends some configuration commands
+
+TODO:
+- Verify device has booted after each configuration command has been sent
+- Confirm setting with appropriate read command and parse response
+- Decide how to actually configure things
+
+
+Things we definitely want to configure:
+    Navigation mode = Airborn
+    Bitrate = 115200
+    Binary mode (maybe?)
+    Update rate = 20 Hz
+    Disable SAEE
+
+Venus 8 ROM versions:
+    Kernel Version 2.0.2
+    Software Version 1.8.27
+    Revision 2013.2.21
+"""
+
+@contextmanager
+def tcp(ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('', 0))
+    sock.connect((ip, port))
+    yield sock
+    sock.close()
+
+def cmd(string):
+    string = format_data(string)
+    with tcp(b'10.10.10.40', 23) as sock:
+        data = '{}\r\n'.format(string).encode('ascii', 'ignore')
+        sock.send(data)
+
+def format_data(data):
+    check = 0
+    string = pack(">HH", 0xA0A1, len(data))
+    for byte in data:
+        check ^= byte
+        string += pack("B", byte)
+    string += pack(">BH", check, 0x0D0A)
+    return string
+
+
+cmd([0x05, 0x00, 0x05, 1])	# set baud 115200
+#cmd([0x64, 0x17, 5, 1])	# Navigation mode = airborne
+#cmd([0x63, 0x01, 0, 1])	# Disable SAEE
+#cmd([0x0E, 20, 1])[30:]	# 20Hz
